374c2aaab9e8dce45a206697b4fee09729db6383
Peter.DeRijk <Peter.DeRijk@gmail.com>
2010-01-07 17:37:24 +0000

    compare changes: added compare_selectivity, changes proc names, count from table

--- lib/compstats.tcl
+++ lib/compstats.tcl
@@ -1,9 +1,9 @@
-proc pvtcomstats {} {
+proc compare_pvt {} {
 	set f stdin
 	# set f [open $compar_file]
 	unset -nocomplain a
 	set header [split [gets $f] \t]
-	set fields {compar sample chromosome type dbsnp ns lowscore loc effect refcons trf str rp sd sc}
+	set fields {compar sample chromosome type dbsnp ns lowscore loc effect refcons trf str rp sd sc a100 a70 b20 b30}
 	set poss [list_cor $header $fields]
 	set nf [list_find $poss -1]
 	if {[llength $nf]} {
@@ -54,70 +54,72 @@ proc comstats_filter {slist cfilter} {
 	return $result
 }
 
-proc count {var slist {cfilter {}}} {
-	upvar $var a
+proc count {slist {cfilter {}}} {
 	if {[llength $cfilter]} {
 		set slist [comstats_filter $slist $cfilter]
 	}
-	set result 0
-	foreach line $slist {
-		incr result [get a($line) 0]
-	}
+	if {![llength $slist]} {return 0}
+	set end [expr {[llength [lindex $slist 0]]-1}]
+	set result [format %0.0f [lmath_sum [list_subindex $slist $end]]]
 	return $result
 }
 
-proc comstats {} {
+proc compare_pvtsummary {} {
 	global header
 	set f stdin
 	# set f [open $file]
-	unset -nocomplain a
 	set header [split [gets $f] \t]
 	set num 0
+	set table {}
 	while {![eof $f]} {
 		incr num
 		if {![expr {$num%100000}]} {puts stderr $num}
 		set line [split [gets $f] \t]
 		if {![llength $line]} continue
-		set num [list_pop line]
-		set a($line) $num
+		lappend table $line
 	}
+	set len [llength $header]
+	set end [expr {$len-1}]
 	set o stdout
+	# selectivity
+	compare_selectivity $o $table
+	# filters
 	array set trans {
 		COMPATIBLE 1 MISSENSE 2 DELETE 3 INSERT 4 DELETE+ 5 INSERT+ 6 NONSTOP 7 NONSENSE 8 FRAMESHIFT 9
 	}
 	set pos [lsearch $header sample]
-	# set idlist [list_remdup [list_subindex [array names a] $pos]]
-	set idlist [list_remdup [split [list_remdup [list_subindex [array names a] $pos]] " ,"]]
+	set idlist [list_remdup [split [list_remdup [list_subindex $table $pos]] " ,"]]
 	set filterlist {0 1 2 3}
 	set loclist {EXON UTR BEGIN END INTRON OTHER ""}
 	set effectlist {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE MISSENSE COMPATIBLE UNDEFINED OTHER}
 	# set o [open $ofile a]
 	set o stdout
-	set alllist [array names a]
 	foreach {title filters} {
 		nofilter {}
 		"filter 1 (removed ref-(in)consistent)" {refcons ""}
 		"filter 3 (removed ref-(in)consistent, ?/N)" {refcons "" ns ""}
 		"filter 4 (removed ref-(in)consistent, ?/N,  scores < 60)" {refcons "" ns "" lowscore ""}
 		"filter 5 (removed ref-(in)consistent, ?/N, scores < 60, tandem repeats)" {refcons "" ns "" lowscore "" trf ""}
+		"filter 5b (removed ref-(in)consistent, ?/N, coverage between 20 and 100, tandem repeats)" {refcons "" ns "" a100 "" b20 "" trf ""}
 		"filter 6 (removed ref-(in)consistent, ?/N, scores < 60, tandem repeats, str)" {refcons "" ns "" lowscore "" trf "" str ""}
 		"filter 7 (removed ref-(in)consistent, ?/N, scores < 60, tandem repeats, str, repeats)" {refcons "" ns "" lowscore "" trf "" str "" rp ""}
 		"filter 8 (removed ref-(in)consistent, ?/N, scores < 60, tandem repeats, str, repeats, segdup)" {refcons "" ns "" lowscore "" trf "" str "" rp "" sd ""}
 		"filter 9 (removed ref-(in)consistent, ?/N, scores < 60, tandem repeats, str, repeats, segdup, self chained)" {refcons "" ns "" lowscore "" trf "" str "" rp "" sd "" sc ""}
+		"filter 9b (removed ref-(in)consistent, ?/N, coverage between 20 and 100, tandem repeats, str, repeats, segdup, self chained)" {refcons "" ns "" a100 "" b20 "" trf "" str "" rp "" sd "" sc ""}
+		"filter 10 (removed ref-(in)consistent, ?/N, scores < 60, coverage between 20 and 100, tandem repeats, str, repeats, segdup, self chained)" {refcons "" ns "" lowscore "" a100 "" b20 "" trf "" str "" rp "" sd "" sc ""}
 	} {
 		puts stderr "===== $title ====="
 		puts $o "\n ==================== $title ===================="
-		set flist [comstats_filter $alllist $filters]
+		set flist [comstats_filter $table $filters]
 		foreach compar {df mm sm} {
 			puts stderr $compar
 			puts $o "\n---------- $compar: $title ----------"
 			set list [comstats_filter $flist [list compar $compar]]
-			set checkall 0
-			foreach name $list {incr checkall $a($name)}
+			set checkall [format %0.0f [lmath_sum [list_subindex $list $end]]]
 			puts $checkall
-			set all [count a $list]
+			set all [count $list]
 			puts $o "total:\t$all"
-			set snps [count a $list {type snp}]
+			set snps [count $list {type snp}]
 			puts $o "snps:\t$snps ([expr {$all-$snps}] other)"
 			if {$compar eq "df"} {
 				set idlistheader \t\t[join $idlist \t]
@@ -126,12 +128,12 @@ proc comstats {} {
 			}
 			puts $o "\nloc\ttotal\tnew\tdbsnp$idlistheader"
 			foreach l $loclist {
-				set temp1 [count a $list [list dbsnp "" loc $l]]
-				set temp2 [count a $list [list dbsnp dbsnp loc $l]]
+				set temp1 [count $list [list dbsnp "" loc $l]]
+				set temp2 [count $list [list dbsnp dbsnp loc $l]]
 				set oline "$l:\t[expr {$temp1+$temp2}]\t${temp1}\t${temp2}\t"
 				if {$compar eq "df"} {
 					foreach id $idlist {
-						append oline \t[count a $list [list loc $l sample $id]]
+						append oline \t[count $list [list loc $l sample $id]]
 					}
 				}
 				puts $o $oline
@@ -144,29 +146,62 @@ proc comstats {} {
 			}
 			puts $o $temp
 			foreach e $effectlist {
-				set temp1 [count a $list [list dbsnp "" loc EXON effect $e]]
-				set temp2 [count a $list [list dbsnp dbsnp loc EXON effect $e]]
+				set temp1 [count $list [list dbsnp "" loc EXON effect $e]]
+				set temp2 [count $list [list dbsnp dbsnp loc EXON effect $e]]
 				set oline "$e\t[expr {$temp1+$temp2}]\t$temp1\t$temp2\t"
 				if {$compar eq "df"} {
 					foreach id $idlist {
-						append oline \t[count a $list [list dbsnp "" loc EXON effect $e sample $id]]
-						append oline \t[count a $list [list dbsnp "" loc EXON effect $e sample $id]]
+						append oline \t[count $list [list dbsnp "" loc EXON effect $e sample $id]]
+						append oline \t[count $list [list dbsnp "" loc EXON effect $e sample $id]]
 					}
 				}
 				puts $o $oline
 			}
-			set temp1 [count a $list [list dbsnp "" loc EXON effect {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE}]]
-			set temp2 [count a $list [list dbsnp dbsnp loc EXON effect {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE}]]
+			set temp1 [count $list [list dbsnp "" loc EXON effect {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE}]]
+			set temp2 [count $list [list dbsnp dbsnp loc EXON effect {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE}]]
 			set oline "*BAD*\t[expr {$temp1+$temp2}]\t$temp1\t$temp2\t"
 			if {$compar eq "df"} {
 				foreach id $idlist {
-					append oline \t[count a $filters [list dbsnp "" loc EXON effect {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE} sample $id]]
-					append oline \t[count a $filters [list dbsnp dbsnp loc EXON effect {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE} sample $id]]
+					append oline \t[count $filters [list dbsnp "" loc EXON effect {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE} sample $id]]
+					append oline \t[count $filters [list dbsnp dbsnp loc EXON effect {FRAMESHIFT NONSENSE NONSTOP INSERT+ DELETE+ INSERT DELETE} sample $id]]
 				}
 			}
 			puts $o $oline
 		}
 	}
+}
+
+proc compare_selectivity {o table} {
+	global header
+	set len [llength [lindex $table 0]]
+	set end [expr {$len-1}]
+	set dflist [comstats_filter $table [list compar df]]
+	set mmlist [comstats_filter $table [list compar mm]]
+	set smlist [comstats_filter $table [list compar sm]]
+	set dftotal [lmath_sum [list_subindex $dflist $end]]
+	set mmtotal [lmath_sum [list_subindex $mmlist $end]]
+	set smtotal [lmath_sum [list_subindex $smlist $end]]
+	puts $o "---------- total ----------"
+	puts $o "df\t[format %0.0f $dftotal]"
+	puts $o "mm\t[format %0.0f $mmtotal]"
+	puts $o "sm\t[format %0.0f $smtotal]"
+	foreach {sfield svalue} {
+		type snp dbsnp dbsnp ns n? lowscore ls 
+		refcons rc trf trf str str rp rp sd sd sc sc 
+		a100 a100 a70 a70 b20 b20 b30 b30
+	} {
+		set list [comstats_filter $dflist [list $sfield $svalue]]
+		set dfselnum [lmath_sum [list_subindex $list $end]]
+		set list [comstats_filter $mmlist [list $sfield $svalue]]
+		set mmselnum [lmath_sum [list_subindex $list $end]]
+		set list [comstats_filter $smlist [list $sfield $svalue]]
+		set smselnum [lmath_sum [list_subindex $list $end]]
+		puts $o "---------- $sfield == $svalue ----------"
+		puts $o "df\t[format %0.0f $dfselnum]\t[format %.2f [expr {100.0*$dfselnum/$dftotal}]]"
+		puts $o "mm\t[format %0.0f $mmselnum]\t[format %.2f [expr {100.0*$mmselnum/$mmtotal}]]"
+		puts $o "sm\t[format %0.0f $smselnum]\t[format %.2f [expr {100.0*$smselnum/$smtotal}]]"
+	}
+
 }
 
 if 0 {
@@ -182,9 +217,6 @@ cd /complgen/compar
 set file 78vs79_mismatch.tsv
 set ofile summary.tsv
 catch {file delete summary.tsv}
-comstats summary.tsv diff.tsv
-comstats summary.tsv same.tsv
-comstats summary.tsv uniqueseq.tsv
 
 set file compar/78vs79_compar_pvt.tsv
 
