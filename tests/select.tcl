0c7354241408d5450bd1051f4b68c40c30cc3308
Peter.DeRijk <Peter.DeRijk@gmail.com>
2010-07-01 08:43:28 +0000

    added tests (select)

---
+++ tests/select.tcl
@@ -0,0 +1,57 @@
+#!/bin/sh
+# the next line restarts using wish \
+exec tclsh "$0" "$@"
+
+source tools.tcl
+
+test select {-f} {
+	exec cg select -f "num text" table.tsv
+} {num	text
+4	a
+10	b
+2	c
+100	d}
+
+test select {-q} {
+	exec cg select -q {$num <= 4} table.tsv
+} {num	text	mixed	other
+4	a	a4	aaaa
+2	c	a2	cc}
+
+test select {-s} {
+	exec cg select -s num table.tsv
+} {num	text	mixed	other
+2	c	a2	cc
+4	a	a4	aaaa
+10	b	b2	bbbbbbbbbb
+100	d	aa3	dd..}
+
+test select {-s -f} {
+	exec cg select -s num -f "num mixed" table.tsv
+} {num	mixed
+2	a2
+4	a4
+10	b2
+100	aa3}
+
+test select {-s -q} {
+	exec cg select -s num -q {$num <= 4} table.tsv
+} {num	text	mixed	other
+2	c	a2	cc
+4	a	a4	aaaa}
+
+test select {-q -f} {
+	exec cg select -f "num mixed" -q {$num <= 4} table.tsv
+} {num	mixed
+4	a4
+2	a2}
+
+test select {-s -f -q} {
+	exec cg select -s num  -f "num mixed" -q {$num <= 4} table.tsv
+} {num	mixed
+2	a2
+4	a4}
+
+
+
+testsummarize
---
+++ tests/tools.tcl
@@ -0,0 +1,7 @@
+package require Extral
+catch {tk appname test}
+
+package require pkgtools
+namespace import pkgtools::*
+
+# pkgtools::testleak 100
