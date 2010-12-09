50033dac44b5d2d208c306ae027e27d1f421c31a
Peter.DeRijk <Peter.DeRijk@gmail.com>
2010-12-09 16:59:59 +0000

    process_sv: do Y if available

--- lib/cg_process_sv.tcl
+++ lib/cg_process_sv.tcl
@@ -10,7 +10,7 @@ proc process_sv {cgdir dir dbdir {force 0}} {
 	if {$force || ![file exists $dir/sv/${name}_map2sv_sort_FINISHED]} {
 		cg map2sv $cgdir $dir/sv/$name
 	}
-	foreach chr {1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X M} {
+	foreach chr {1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X M Y} {
 		set file $dir/sv/$name-$chr-paired.tsv
 		set root [file root $file]
 		if {$force || ![file exists $file]} {
