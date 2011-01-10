f6bc56e7c6b7a50ed0e1faec8da360fd9cc1e72d
Peter.DeRijk <Peter.DeRijk@gmail.com>
2011-01-10 10:51:16 +0000

    added cg_sh

---
+++ lib/cg_sh.tcl
@@ -0,0 +1,27 @@
+#!/bin/sh
+# the next line restarts using wish \
+exec tclsh "$0" ${1+"$@"}
+
+package require Extral
+
+proc cg_sh {args} {
+	package require Tclx
+	signal -restart error SIGINT
+	if {[info commands "console"] == "console"} {
+		console show
+	} else {
+		uplevel #0 {commandloop -prompt1 {puts -nonewline "% "} -prompt2 {puts -nonewline ""}}
+	}
+}
+
+if {[info exists argv]} {
+	set scriptname [info script]
+	package require pkgtools
+	set appdir [file dir [pkgtools::startdir]]
+	lappend auto_path $appdir/lib
+	append env(PATH) :[file dir [file dir $appdir]]/bin:$appdir/bin
+	package require Extral
+	set ::base $scriptname
+	cg_sh {*}$argv
+}
+
