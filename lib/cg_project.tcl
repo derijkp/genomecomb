a6d21bc6e4b3e8dc61c21810f8a9c03dead1d44a
Peter.DeRijk <Peter.DeRijk@gmail.com>
2011-02-03 17:19:40 +0000

    added cg_project

---
+++ lib/cg_project.tcl
@@ -0,0 +1,271 @@
+#!/bin/sh
+# the next line restarts using wish \
+exec tclsh "$0" ${1+"$@"}
+
+package require Extral
+
+proc submit {args} {
+	set host {}
+	set deps {}
+	set submitargs {}
+	while 1 {
+		set key [lindex $args 0]
+		if {$key eq "-host"} {
+			set host [lindex $args 1]
+			if {[llength $host]} {
+				lappend submitargs -host $host
+			}
+			set args [lrange  $args 2 end]
+		} elseif {$key eq "-deps"} {
+			set deps [lindex $args 1]
+			if {[llength $deps]} {
+				lappend submitargs -deps $deps
+			}
+			set args [lrange  $args 2 end]
+		} else {
+			break
+		}
+	}
+	puts "Submitting: $submitargs $args"
+	set error [catch {
+		set temp [eval exec submit.tcl $submitargs $args]
+	} result]
+	if {$error} {
+		if {[regexp {is running, skipping} $result]} {
+			puts $result
+			return ""
+		} else {
+			error $result $::errorInfo
+		}
+	}
+	puts $result
+	if {[regexp {[0-9]+} $temp job]} {
+		return $job
+	} else {
+		return ""
+	}
+}
+
+proc mcompar_samples {file} {
+	set header [cg select -h $file]
+	set poss [list_find -glob $header sequenced-*]
+	set done {}
+	foreach el [list_sub $header $poss] {
+		lappend done [string range $el 10 end]
+	}
+	return $done
+}
+
+proc cg_project {args} {
+	if {([llength $args] < 3)} {
+		puts stderr "format is: $::base project_file refseqdir action/option ..."
+		puts stderr " - processes project according to project file"
+		puts stderr " - actions: samples, compar, sv, clean, users"
+		puts stderr " - options: direct"
+		exit 1
+	}
+	foreach {projectfile refseqdir} $args break
+	set actions [lrange $args 2 end]
+	set pos [lsearch $actions direct]
+	set direct 0
+	if {$pos != -1} {
+		set actions [list_remove $actions direct]
+		set direct 1
+	}
+	if {[llength [list_lremove $actions {samples compar sv clean users}]]} {
+		error "unkown action(s) [join [list_lremove $actions {samples compar sv clean users}] ,], must be one or more of: samples compar sv clean users"
+	}
+	set projectfile [file normalize $projectfile]
+	if {[file isdir $projectfile]} {set projectfile [lindex [glob $projectfile/*.cgprj] 0]}
+	puts "Projectfile $projectfile"
+	set projectdir [file dir $projectfile]
+	set project [file root [file tail $projectfile]]
+	set resultdir [file dir [file normalize $projectfile]]
+	set c [split [file_read $projectfile] \n]
+	set pos [lsearch $c {}]
+	array set a [list_concat [lrange $c 0 [expr {$pos-1}]]]
+	set build [get a(build) hg18]
+	set data [list_remove [lrange $c [expr {$pos+1}] end] {}]
+	set poss [list_find -glob $data {#*}]
+	set data [join [list_sub $data -exclude $poss] \n]
+	puts "data:\n$data"
+
+	# samples
+	# =======
+	puts "Resultdir: $resultdir"
+	file mkdir $resultdir
+	cd $resultdir
+	set alljobs {}
+	set jobs {}
+	if {[inlist $actions samples] || [inlist $actions sample]} {
+		foreach {cgdir name} $data {
+			set dir [file dir $cgdir]
+			file mkdir $name
+			if {$direct} {
+				puts "Processing $name"
+				exec cg process_sample $cgdir $name $refseqdir >@ stdout 2>@stderr
+			} else {
+				set host [lindex [file split $cgdir] 2]
+				set job [submit -host $host cg process_sample $cgdir $name $refseqdir]
+				if {[isint $job]} {lappend jobs $job}
+			}
+			catch {file copy $cgdir/ASM/CNV $name} result
+			puts $result
+			catch {file copy $cgdir/ASM/SV $name} result
+			puts $result
+		}
+		lappend alljobs {*}$jobs
+	}
+#	cd $resultdir
+#	foreach {cgdir name} $data {
+#		set dir [file dir $cgdir]
+#		catch {exec ln -s $dir/$name .}
+#		file mkdir ori
+#		catch {exec ln -s $cgdir ori}
+#		catch {exec ln -s $cgdir ori/$name.ori}
+#	}
+	
+	# multicompar
+	# ===========
+	if {[inlist $actions compar]} {
+		set names {}
+		foreach {cgdir name} $data {
+			lappend names $name
+		}
+		if {[file exists compar/${project}_compar.tsv]} {
+			set done [mcompar_samples compar/${project}_compar.tsv]
+			set names [list_lremove $names $done]
+		} else {
+			set done {}
+		}
+		if {[llength $names]} {
+			file mkdir compar
+			if {$direct} {
+				puts "Multicompar: adding $name"
+				exec cg multicompar -reannot compar/${project}_compar.tsv {*}$names >@ stdout 2>@stderr
+				exec cg annotate compar/${project}_compar.tsv compar/annot${project}_compar.tsv $refseqdir/$build $refseqdir/annovar >@ stdout 2>@stderr
+			} else {
+				set cjob [submit -host lungo -deps [join $jobs ,] cg multicompar -reannot compar/${project}_compar.tsv {*}$names]
+		 		lappend alljobs $cjob
+				set ajob [submit -host lungo -deps $cjob cg annotate compar/${project}_compar.tsv compar/annot${project}_compar.tsv $refseqdir/$build $refseqdir/annovar]
+		 		lappend alljobs $ajob
+			}
+		}
+		# multireg
+		if {[file exists compar/${project}_reg.tsv]} {
+			set done [mcompar_samples compar/${project}_reg.tsv]
+		} else {
+			set done {}
+		}
+		set files {}
+		foreach {cgdir name} $data {
+			if {![inlist $done $name]} {
+				lappend files $name/sreg-$name.tsv
+			}
+		}
+		if {[llength $files]} {
+			if {$direct} {
+				puts "Multireg: adding $name"
+				exec cg multireg compar/${project}_reg.tsv {*}$files >@ stdout 2>@stderr
+			} else {
+				set crjob [submit -deps [join $jobs ,] cg multireg compar/${project}_reg.tsv {*}$files]
+		 		lappend alljobs $crjob
+			}
+		}
+	}
+
+	# sv (our algorithm)
+	# ==================
+	if {[inlist $actions sv]} {
+		cd $resultdir
+		set svjobs {}
+		foreach {cgdir name} $data {
+			set dir [file dir $cgdir]
+			if {$direct} {
+				puts "sv: adding $name"
+				exec cg process_sv $cgdir $name $refseqdir >@ stdout 2>@stderr
+			} else {
+				set host [lindex [file split $cgdir] 2]
+				set job [submit -host $host cg process_sv $cgdir $name $refseqdir]
+				lappend svjobs $job
+			}
+		}
+		lappend alljobs {*}$svjobs
+		# qalter -u peter -p 1
+	}
+
+	# cleanup/compress
+	# ================
+	if {[inlist $actions clean]} {
+		cd $resultdir
+		foreach {cgdir name} $data {
+			set host [lindex [file split $cgdir] 2]
+			set dir [file dir $cgdir]
+			set files [glob -nocomplain $name/sv/*-paired.tsv]
+			if {[llength $files]} {
+				if {$direct} {
+					exec razip {*}$files >@ stdout 2>@stderr
+				} else {
+					set job [submit -host $host -deps $alljobs razip {*}$files]
+				}
+			}
+		}
+	}
+
+	# users
+	# =====
+	if {[inlist $actions users]} {
+		cd $resultdir
+		foreach user [get a(users) ""] {
+			puts "$project -> $user"
+			if {![file exists /home/MOLGEN/$user/complgen]} {
+				catch {file mkdir /home/MOLGEN/$user/complgen}
+				exec chown "$user.domain users" /home/MOLGEN/$user/complgen
+			}
+			cd /home/MOLGEN/$user/complgen
+			if {![file exists docs]} {	
+				exec ln -s /complgen/projects/docs .
+			}
+			if {![file exists $project]} {
+				exec ln -s $projectdir .
+			}
+		}
+	}
+}
+
+if 0 {
+	foreach {cgdir name} $data {
+		set dir [file dir $cgdir]
+		cg select -q {$size > 150} $name/sv/$name-1-paired-sv.tsv $name/sv-$name.tsv
+		foreach chr {2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 M X Y} {
+			catch {file delete temp}
+			catch {
+				cg select -q {$size > 150} $name/sv/$name-$chr-paired-sv.tsv temp
+				exec tail -n +2 temp >> $name/sv-$name.tsv
+			} e
+			puts $e
+		}
+	}
+	set names {}
+	foreach {cgdir name} $data {
+		lappend names $name
+	}
+	file mkdir compar
+	if {[info exists svjobs] && [llength $svjobs]} {
+		set temp [eval {exec submit.tcl -host lungo -deps [join $svjobs ,] cg svmulticompar compar/${project}_svcompar.tsv} $names]
+	} else {
+		set temp [eval {exec submit.tcl -host lungo cg svmulticompar compar/${project}_compar.tsv} $names]
+	}
+
+}
+
+if {[info exists argv]} {
+	package require pkgtools
+	set appdir [file dir [pkgtools::startdir]]
+	lappend auto_path $appdir/lib
+	append env(PATH) :[file dir [file dir $appdir]]/bin:$appdir/bin
+	package require Extral
+	set ::base $scriptname
+	cg_project {*}$argv
+}
+
