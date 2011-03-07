b0a8cb03879998b0bdd00d9635d65bcaf7381994
Peter.DeRijk <Peter.DeRijk@gmail.com>
2011-03-07 15:37:31 +0000

    cg groupby changes: list of fields is one arg, take infile and outfile, separate help

--- lib/cg_groupby.tcl
+++ lib/cg_groupby.tcl
@@ -11,20 +11,34 @@ cg groupby s1 < ~/dev/completegenomics/tests/table2.tsv
 cg select -q '$symbol != ""' /complgen/projects/test/annottest_compar.tsv | cg groupby symbol > /complgen/projects/test/groupby_symbol_test_compar.tsv
 }
 
+proc cg_groupby_help {} {
+set help [file_read $::appdir/lib/cg_groupby.help]
+puts [string_change $help [list @BASE@ [get ::base {[info source]}]]]
+}
 
 proc cg_groupby {args} {
 	if {([llength $args] < 1)} {
-		puts stderr "format is: $::base ?field? ..."
-		puts stderr " - groups lines in a tab separated file (from stdin), where the value in columns given by field are the same"
-		puts stderr " - entries are concatenated using commas"
+		puts "Wrong number of arguments"
+		cg_groupby_help
 		exit 1
 	}
-	set fields $args
-	# set f [rzopen $file]
-	set f stdin
+	set fields [lindex $args 0]
+	set args [lrange $args 1 end]
+	if {[llength $args] > 0} {
+		set filename [lindex $args 0]
+		set f [rzopen $filename]
+	} else {
+		set f stdin
+	}
+	if {[llength $args] > 1} {
+		set outfile [lindex $args 1]
+		set o [open $outfile w]
+	} else {
+		set o stdout
+	}
 	set header [tsv_open $f]
 	set poss [list_cor $header $fields]
-	puts [join [list_concat $fields [list_sub $header -exclude $poss]] \t]
+	puts $o [join [list_concat $fields [list_sub $header -exclude $poss]] \t]
 	set line [split [gets $f] \t]
 	set prev [list_sub $line $poss]
 	set grouped $line
@@ -36,7 +50,7 @@ proc cg_groupby {args} {
 		if {$num >= $next} {putslog $num; incr next 100000}
 		set cur [list_sub $line $poss]
 		if {$cur != $prev} {
-			puts [join [list_concat $prev [list_sub $grouped -exclude $poss]] \t]
+			puts $o [join [list_concat $prev [list_sub $grouped -exclude $poss]] \t]
 			set grouped $line
 			set prev $cur
 		} else {
@@ -47,8 +61,9 @@ proc cg_groupby {args} {
 			set grouped $temp
 		}
 	}
-	puts [join [list_concat $prev [list_sub $grouped -exclude $poss]] \t]
-	# close $f
+	puts $o [join [list_concat $prev [list_sub $grouped -exclude $poss]] \t]
+	if {$o ne "stdout"} {catch {close $o}}
+	if {$f ne "stdin"} {catch {close $f}}
 }
 
 if {[info exists argv]} {
