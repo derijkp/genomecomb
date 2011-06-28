a1b730c0c354668e79c148e71fc437320747b466
Peter.DeRijk <Peter.DeRijk@gmail.com>
2011-06-28 17:41:20 +0000

    cg_makepvt fix: work on non-region files

--- lib/pvt.tcl
+++ lib/pvt.tcl
@@ -25,17 +25,25 @@ proc cg_makepvt {args} {
 		exit 1
 	}
 	foreach {file resultfile fields} $args break
+	set h [cg select -h $file]
 	if {$fields eq ""} {
-		set fields [list_remove [cg select -h $file] chromosome begin end]
+		set fields [list_remove $h chromosome begin end]
 	}
 	set ufields $fields
-	lappend ufields {numbases=$end - $begin} {numlines=1}
+	if {[lsearch [list_find $h {chromosome begin end}] -1] != -1} {
+		set region 1
+		lappend ufields {numbases=$end - $begin} {numlines=1}
+	} else {
+		set region 0
+	}
 	cg select -f $ufields $file $tempfile
 	if {$sorted} {
 		set tempfile2 [tempfile]
 		cg select -s $fields $tempfile $tempfile2
 		set tempfile $tempfile2
 	}
-	lappend sumfields numbases numlines
+	if {$region} {
+		lappend sumfields numbases numlines
+	}
 	cg groupby -sorted $sorted -sumfields $sumfields $fields $tempfile $resultfile
 }
