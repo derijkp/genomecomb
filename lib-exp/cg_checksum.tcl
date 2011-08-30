f43ac2ae8291e4418d5ae0bbc7ce64c251c71139
Peter.DeRijk <Peter.DeRijk@gmail.com>
2011-08-30 10:03:14 +0000

    added lib-exp/cg_checksum

---
+++ lib-exp/cg_checksum.tcl
@@ -0,0 +1,53 @@
+#proc checksumdir {basedir o dir} {
+#	set files [dirglob $basedir [file join $dir *]]
+#	
+#	foreach file $files {
+#		if {[file isdir $file]} {
+#			checksumdir $basedir $o $file
+#		} else {
+#			puts "Checking $file"
+##			set sha256 [lindex [exec sha256sum $file] 0]
+#set sha256 test
+#			puts $o "$sha256 *$file"
+#		}
+#	}
+#}
+
+proc cg_checksum {args} {
+	if {[llength $args] < 1} {
+		error "format is: cg checksum dir\n or: cg checksum manifestfile manifestfile ..."
+		exit 1
+	}
+	if {[llength $args] == 1} {
+		set basedir [lindex $args 0]
+		if {[file isdir $basedir]} {
+			set files [glob $basedir/manifest.all $basedir/*/manifest.all $basedir/*/*/manifest.all]
+			if {[llength $files] == 0} {
+				set files [exec find $basedir -iname manifest.all]
+			}
+		} else {
+			set files $args
+		}
+	} else {
+		set files $args
+	}
+	set numok 0
+	set numfailed 0
+	foreach file $files {
+		set dir [file dir $file]
+		cd $dir
+		if {![file exists checksum.txt]} {
+			puts "Checking $file"
+			set error [catch {exec sha256sum -c manifest.all > checksum.txt} result]
+		}
+		if {![catch {exec grep FAILED checksum.txt}]} {
+			incr numfailed
+			puts "FAILED $file"
+		} else {
+			incr numok
+			puts "OK $file"
+		}
+	}
+	puts "ok: $numok"
+	puts "failed: $numfailed"
+}
