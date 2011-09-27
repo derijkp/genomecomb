b0de76d7f4112168f36d9e2a5b74949654a89982
Peter.DeRijk <Peter.DeRijk@gmail.com>
2011-09-27 13:10:53 +0000

    cg_convcgsv and cg_convcgcnv moved to lib

---
+++ lib/cg_convcgcnv.tcl
@@ -0,0 +1,22 @@
+#
+# Copyright (c) by Peter De Rijk (VIB - University of Antwerp)
+# See the file "license.txt" for information on usage and redistribution of
+# this file, and for a DISCLAIMER OF ALL WARRANTIES.
+#
+
+proc convcgcnv {srcfile dstfile} {
+	set fields [cg select -h $srcfile]
+	set nfields [list_concat {chr begin end {type=$calledCNVType}} [list_remove $fields chr begin end calledCNVType]]
+	cg select -f $nfields -q {$calledCNVType != "hypervariable" && $calledCNVType != "\=" && $calledCNVType != "invariant"} $srcfile $dstfile.temp
+	file rename -force $dstfile.temp $dstfile
+}
+
+proc cg_convcgcnv {args} {
+	if {[llength $args] != 2} {
+		errorformat convcgcnv
+		exit 1
+	}
+	foreach {srcfile dstfile} $args break
+	convcgcnv $srcfile $dstfile.temp
+	file rename $dstfile.temp $dstfile
+}
