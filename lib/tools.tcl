5216cbed41c03cf80136789c0c31a104e42880c8
Peter.DeRijk <Peter.DeRijk@gmail.com>
2011-12-21 13:41:52 +0000

    added cg_razip

--- lib/tools.tcl
+++ lib/tools.tcl
@@ -219,6 +219,55 @@ proc cg_bgzip args {
 	}
 }
 
+proc cg_razip args {
+	set pos 0
+	set keep 0
+	foreach {key value} $args {
+		switch -- $key {
+			-k {
+				set keep 1
+			}
+			default {
+				break
+			}
+		}
+		incr pos 2
+	}
+	set args [lrange $args $pos end]
+	foreach file $args {
+		set ext [file extension $file]
+		switch $ext {
+			.gz {
+				putslog "razip $file"
+				set result [file root $file].rz
+				exec gunzip -d -c $file > $result.temp2
+				exec razip -c $result.temp2 > $result.temp
+				file delete $result.temp2
+				file rename $result.temp $result
+				if {!$keep} {file delete $file}
+			}
+			.rz {
+				putslog "$file already razip"
+			}
+			.bz2 {
+				putslog "razip $file"
+				set result [file root $file].rz
+				exec bzcat $file > $result.temp2
+				exec razip -c $result.temp2 > $result.temp
+				file delete $result.temp2
+				file rename $result.temp $result
+				if {!$keep} {file delete $file}
+			}
+			default {
+				putslog "razip $file"
+				exec razip -c $file > $file.rz.temp
+				file rename $file.rz.temp $file.rz
+				if {!$keep} {file delete $file}
+			}
+		}
+	}
+}
+
 proc gzopen {file {pos -1}} {
 	if {[inlist {.rz} [file extension $file]]} {
 		if {$pos == -1} {
