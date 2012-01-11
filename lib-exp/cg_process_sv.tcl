25606171836e1b99a2ad80cd9115689ce7c9b3bc
Peter.DeRijk <Peter.DeRijk@gmail.com>
2012-01-11 12:28:30 +0000

    process_sv fix: take correct root when paired files are already compressed (exp)

--- lib-exp/cg_process_sv.tcl
+++ lib-exp/cg_process_sv.tcl
@@ -20,7 +20,7 @@ proc process_sv {cgdir dir dbdir {force 0}} {
 	foreach chr {1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X M Y} {
 		lappend resultfiles $dir/sv/$name-$chr-paired-sv.tsv
 		set file [gzfile $dir/sv/$name-$chr-paired.tsv]
-		set root [file root $file]
+		set root [file root [gzroot $file]]
 		if {$force || ![file exists $file]} {
 			set file $file.rz
 		}
