b04d5d585917a13b53f67889c09c2dfd0fdbef59
Peter.DeRijk <Peter.DeRijk@gmail.com>
2012-01-24 18:12:03 +0000

    cg_convcgcnv change: use del and amp for type

--- lib/cg_convcgcnv.tcl
+++ lib/cg_convcgcnv.tcl
@@ -6,7 +6,7 @@
 
 proc convcgcnv {srcfile dstfile} {
 	set fields [cg select -h $srcfile]
-	set nfields [list_concat {chr begin end {type=$calledCNVType}} [list_remove $fields chr begin end calledCNVType]]
+	set nfields [list_concat {chr begin end {type=($calledCNVType == "-")? "del" : (($calledCNVType == "+")? "amp" : $calledCNVType)}} [list_remove $fields chr begin end calledCNVType]]
 	cg select -f $nfields -q {$calledCNVType != "hypervariable" && $calledCNVType != "\=" && $calledCNVType != "invariant"} $srcfile $dstfile.temp
 	file rename -force $dstfile.temp $dstfile
 }
