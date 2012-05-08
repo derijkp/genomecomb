4503e3b06daf2040d3ac73f59adf9e26141b0890
Peter.DeRijk <Peter.DeRijk@gmail.com>
2012-05-08 10:57:56 +0000

    skip some tests in tests/val.tcl (no longer valid with new refdb data)

--- tests/val.tcl
+++ tests/val.tcl
@@ -7,7 +7,7 @@ source tools.tcl
 # cg select -q 'oneof($locus,1224,1226,1598520,2816907,2816911,2818387,3038185,3042129,5019241,5054443,5211957,9605719,10679396,19364044,21233191,22632054,21542413)' cgNA19240/fannotvar-cgNA19240.tsv > testvars.tsv
 
 puts "expected data no longer valid with new dbsnp"
-exit
+# exit
 
 test makesequenom {basic} {
 	exec cg makesequenom data/testvars.tsv temp.sft /complgen/refseq/hg19 2> /dev/null
