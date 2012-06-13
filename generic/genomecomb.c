56d6625fb017d6bb739ed58105345d078ee7112a
Peter.DeRijk <Peter.DeRijk@gmail.com>
2012-06-13 09:01:22 +0000

    tsv_selectc wip: calcualted columns as procs in outcolumns, queryproc as parameter, info,

--- generic/genomecomb.c
+++ generic/genomecomb.c
@@ -15,25 +15,42 @@
 #include "tools.h"
 #include "genomecomb.h"
 
+/*
+ * 
+ * tsv_selectc queryproc varcolumns outcolumns
+ * this proc goes over stdin line by line interpreting it as a tab seperated file (no "", fields just cannot contain tabs)
+ * for each line queryproc is called with the values in columns given by varcolumns (numbers) as parameters
+ * if the result is true, output for this line is generated
+ * outcolumns contains a list of columns (indicated by number) to output
+ * if an element in the list is not an integer number, it will be used as a function that is called
+ * with the same parameters as queryproc, and the result is put in the output column
+ * If outcolumns is empty, output will consist of the entire line
+ *
+ * This is a rather dirty function, for speed reasons:
+ * It just accesses stdin and stdout directly, bypassing Tcl channels completely
+ * It uses non-threadsafe file access (getc_unlocked), and bytearrays.
+ * All this makes it order of magnitudes faster than going the clean way through Tcl
+ */
+
 int 
 genomecomb_tsv_select_ObjCmd (ClientData clientData,	Tcl_Interp *interp, int argc, Tcl_Obj *CONST argv[])
 {
 	/* Dstring */
-	Tcl_CmdInfo cmdinfo;
-	Tcl_Obj **objv,*queryresult;
-	int listobjc,listoutc,listresc,*cols=NULL,*outs=NULL;
-	Tcl_Obj **listobjv,**listoutv,**listresv;
+	Tcl_CmdInfo cmdinfo,cmdinfo2;
+	Tcl_Obj **objv,*queryresult,*querycmd=NULL;
+	int listobjc,listoutc,*cols=NULL,*outs=NULL;
+	Tcl_Obj **listobjv,**listoutv;
 	DString *line = NULL,*array;
 	ssize_t read;
 	int maxtab=0,objc,show,i,error;
-	if ((argc < 3)||(argc > 3)) {
-		Tcl_WrongNumArgs(interp, 1, argv, "varcolumns outcolumns");
+	if ((argc < 4)||(argc > 4)) {
+		Tcl_WrongNumArgs(interp, 1, argv, "queryproc varcolumns outcolumns");
 		return TCL_ERROR;
 	}
-	if (Tcl_ListObjGetElements(interp, argv[1], &listobjc, &listobjv) != TCL_OK) {
+	if (Tcl_ListObjGetElements(interp, argv[2], &listobjc, &listobjv) != TCL_OK) {
 		return TCL_ERROR;
 	}
-	if (Tcl_ListObjGetElements(interp, argv[2], &listoutc, &listoutv) != TCL_OK) {
+	if (Tcl_ListObjGetElements(interp, argv[3], &listoutc, &listoutv) != TCL_OK) {
 		return TCL_ERROR;
 	}
 	cols = (int *)Tcl_Alloc(listobjc*sizeof(int));
@@ -45,15 +62,16 @@ genomecomb_tsv_select_ObjCmd (ClientData clientData,	Tcl_Interp *interp, int arg
 	outs = (int *)Tcl_Alloc(listoutc*sizeof(int));
 	for (i = 0 ; i < listoutc ; i++) {
 		error = Tcl_GetIntFromObj(interp,listoutv[i],outs+i);
-		if (error) {return error;}
-		if (outs[i] > maxtab) maxtab = outs[i];
+		if (error) {
+			outs[i] = -1;
+		} else if (outs[i] > maxtab) maxtab = outs[i];
 	}
 	line = DStringNew();
 	array = DStringArrayNew(maxtab+2);
 	objv = (Tcl_Obj **)Tcl_Alloc((listobjc+2)*sizeof(Tcl_Obj *));
-	objv[0]=Tcl_NewStringObj("tsv_selectc_query",17);
-	Tcl_IncrRefCount(objv[0]);
-	i = Tcl_GetCommandInfo(interp,Tcl_GetStringFromObj(objv[0],NULL),&cmdinfo);
+	querycmd = argv[1];
+	objv[0]= querycmd;
+	i = Tcl_GetCommandInfo(interp,Tcl_GetStringFromObj(querycmd,NULL),&cmdinfo);
 	if (i==0) {return TCL_ERROR;}
 	for (i = 1 ; i <= listobjc ; i++) {
 		objv[i] = Tcl_NewByteArrayObj((unsigned char *)"",0);
@@ -64,7 +82,6 @@ genomecomb_tsv_select_ObjCmd (ClientData clientData,	Tcl_Interp *interp, int arg
 	while ((read = DStringGetTab(line,stdin,maxtab,array)) != -1) {
 		if (line->string[0] == '\0') break;
 		NODPRINT("line = %s",line->string);
-		error = cmdinfo.objProc(cmdinfo.objClientData,interp,objc,objv);
 		for (i = 0 ; i < listobjc ; i++) {
 			NODPRINT("col = %d, val=%.*s",cols[i],array[cols[i]].size,array[cols[i]].string);
 			Tcl_SetByteArrayObj(objv[i+1],(unsigned char *)array[cols[i]].string,array[cols[i]].size);
@@ -72,35 +89,42 @@ genomecomb_tsv_select_ObjCmd (ClientData clientData,	Tcl_Interp *interp, int arg
 		error = cmdinfo.objProc(cmdinfo.objClientData,interp,objc,objv);
 		if (error) {return error;}
 		queryresult = Tcl_GetObjResult(interp);
-		if (Tcl_ListObjGetElements(interp, queryresult, &listresc, &listresv) != TCL_OK) {
-			return TCL_ERROR;
-		}
-		if (!listresc) {
-			show = 0;
-		} else {
-			error = Tcl_GetBoolean(interp,Tcl_GetStringFromObj(listresv[0],NULL),&show);
-			if (error) {return error;}
-		}
+		error = Tcl_GetBoolean(interp,Tcl_GetStringFromObj(queryresult,NULL),&show);
+		if (error) {return error;}
 		if (show) {
+			register char *cur;
+			int size;
 			if (listoutc == 0) {
 				fprintf(stdout,"%.*s\n",line->size,line->string);
 			} else {
-				char *sep="";
-				int out,pos=1;
+				int out,first=1;
 				for (i = 0 ; i < listoutc ; i++) {
 					out=outs[i];
+					if (!first) {putc_unlocked('\t',stdout);}
+					first = 0;
 					if (out != -1) {
-						fprintf(stdout,"%s%.*s",sep,array[out].size,array[out].string);
-					} else if (pos < listresc) {
-						fprintf(stdout,"%s%s",sep,Tcl_GetStringFromObj(listresv[pos++],NULL));
+						cur = array[out].string;
+						size = array[out].size;
+						while(size--) {putc_unlocked(*cur++,stdout);}
+					} else {
+						objv[0]=listoutv[i];
+						error = Tcl_GetCommandInfo(interp,Tcl_GetStringFromObj(listoutv[i],NULL),&cmdinfo2);
+						if (error==0) {
+							Tcl_ResetResult(interp);
+							Tcl_AppendResult(interp, "unknown command: \"", Tcl_GetStringFromObj(listoutv[i],NULL), "\"", (char *) NULL);
+							return TCL_ERROR;
+						}
+						error = cmdinfo2.objProc(cmdinfo2.objClientData,interp,objc,objv);
+						if (error) {return error;}
+						queryresult = Tcl_GetObjResult(interp);
+						cur = Tcl_GetStringFromObj(queryresult,&size);
+						while(size--) {putc_unlocked(*cur++,stdout);}
 					}
-					sep="\t";
 				}
-				fprintf(stdout,"\n");
+				putc_unlocked('\n',stdout);
 			}
 		}
 	}
-	Tcl_DecrRefCount(objv[0]);
 	for (i = 1 ; i <= listobjc ; i++) {
 		Tcl_DecrRefCount(objv[i]);
 	}
