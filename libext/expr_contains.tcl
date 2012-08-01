7a930c0f482f72e3ad3d2886b0d7acde02bac4e8
Peter.DeRijk <Peter.DeRijk@gmail.com>
2012-08-01 08:40:18 +0000

    cg_select fix: use same vector separators everywhere (comma semicolon or space)

--- libext/expr_contains.tcl
+++ libext/expr_contains.tcl
@@ -1,6 +1,6 @@
 proc tcl::mathfunc::contains {list value} {
 	set result 1
-	set list [split $list ",'\;"]
+	set list [split $list ";, "]
 	foreach v $list {
 		if {$v eq $value} {
 			return 1
@@ -11,8 +11,8 @@ proc tcl::mathfunc::contains {list value} {
 
 proc tcl::mathfunc::shares {list valuelist} {
 	set result 1
-	set list [split $list ",'\;"]
-	foreach v [split $valuelist ",'\;"] {set a($v) 1}
+	set list [split $list ";, "]
+	foreach v [split $valuelist ";, "] {set a($v) 1}
 	foreach v $list {
 		if {[info exists a($v)]} {
 			return 1
--- libext/expr_count.tcl
+++ libext/expr_count.tcl
@@ -4,7 +4,7 @@ proc tcl::mathop::~ {value pattern} {
 
 proc tcl::mathfunc::hasone {list operand value} {
 	set result 1
-	set list [split $list ",'\;"]
+	set list [split $list ";, "]
 	set cmd tcl::mathfunc::$operand
 	foreach v $list {
 		if {[$cmd $v $value]} {
@@ -16,7 +16,7 @@ proc tcl::mathfunc::hasone {list operand value} {
 
 proc tcl::mathfunc::hasall {list operand value} {
 	set result 1
-	set list [split $list ",'\;"]
+	set list [split $list ";, "]
 	set cmd tcl::mathfunc::$operand
 	foreach v $list {
 		if {![$cmd $v $value]} {
