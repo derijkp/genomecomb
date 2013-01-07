5c144aba17e1f3fdd6d4cb7d7c7af2f6358a38df
Peter.DeRijk <Peter.DeRijk@gmail.com>
2013-01-07 14:15:12 +0000

    added cg_distribute (wip)

---
+++ lib-exp/cg_distribute.tcl
@@ -0,0 +1,28 @@
+# distribute to experiments
+# -------------------------
+
+proc cg_distribute {projectfile srcdir destdir {subdir {}} {postfix {}}} {
+	set srcdir [file normalize $srcdir]
+	set destdir [file normalize $destdir]
+	set c [split [string trim [file_read $projectfile]] \n]
+	set header [split [list_shift c] \t]
+	if {[lrange $header 0 3] ne "project experiment sample barcode"} {
+		error "project-$project.txt must have columns: project experiment sample barcode"
+	}
+	list_foreach {p experiment sample barcode} $c {
+		if {$subdir ne ""} {
+			set dest $destdir/$experiment/$sample$postfix/$subdir
+		} else {
+			set dest $destdir/$experiment/$sample$postfix
+		}
+		file mkdir $dest
+		set files [glob -nocomplain $srcdir/*$barcode]
+		set poss [list_find -regexp $files "^.*\[^0-9\]$barcode\$"]
+		set files [list_sub $files $poss]
+		foreach file $files {
+			putslog "hardlinking $file/* to $dest"
+			exec cp -al {*}[glob $file/*] $dest
+		}
+	}
+
+}
