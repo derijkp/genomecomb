21a71d30f267c8c10195708271f8031dec2fbb22
Peter.DeRijk <Peter.DeRijk@gmail.com>
2013-04-09 11:48:48 +0000

    added cg_nop (for testing)

---
+++ lib-exp/cg_nop.tcl
@@ -0,0 +1,16 @@
+#!/bin/sh
+# the next line restarts using wish \
+exec tclsh "$0" ${1+"$@"}
+
+proc cg_nop args {
+}
+
+if {[info exists argv0] && [file tail [info script]] eq [file tail $argv0]} {
+	package require pkgtools
+	set appdir [file dir [pkgtools::startdir]]
+	lappend auto_path $appdir/lib
+	append env(PATH) :[file dir [file dir $appdir]]/bin:$appdir/bin
+	package require Extral
+	set ::base [file tail [info script]]
+	cg_nop {*}$argv
+}
