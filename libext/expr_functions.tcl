78a8e4cf43dd7463d03c1f814f2815ed7400fa8a
Peter.DeRijk <Peter.DeRijk@gmail.com>
2013-07-22 14:40:07 +0000

    added tcl::mathfunc::sum

--- libext/expr_functions.tcl
+++ libext/expr_functions.tcl
@@ -13,3 +13,19 @@ proc tcl::mathfunc::avg args {
 		expr {double($tot)/$num}
 	}
 }
+
+proc tcl::mathfunc::sum args {
+	set tot 0
+	set num 0
+	foreach v $args {
+		if {[isdouble $v]} {
+			set tot [expr {$tot + $v}]
+			incr num
+		}
+	}
+	if {$num == 0} {
+		return NaN
+	} else {
+		expr $tot
+	}
+}
