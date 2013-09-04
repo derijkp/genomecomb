80d8c21b039f296ac1c3aa1796b9c6214b67e56d
Peter.DeRijk <Peter.DeRijk@gmail.com>
2013-09-04 22:39:58 +0000

    debug.h: add fflush of stdout to DPRINT

--- src/debug.h
+++ src/debug.h
@@ -1,7 +1,7 @@
 /* #define DEBUG 1 */
 
 #ifdef DEBUG
-#define DPRINT(...) fprintf(stderr, "-- ");fprintf(stderr, __VA_ARGS__);fprintf(stderr, "  (%s:%d)\n", __FUNCTION__, __LINE__);fflush(stderr);
+#define DPRINT(...) fprintf(stderr, "-- ");fprintf(stderr, __VA_ARGS__);fprintf(stderr, "  (%s:%d)\n", __FUNCTION__, __LINE__);fflush(stderr);fflush(stdout);
 #define NODPRINT(fmt, ...) /**/
 #else
 #define DPRINT(fmt, ...) /**/
