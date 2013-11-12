6549f7470d906819d33427adb0ce63402a970469
Peter.DeRijk <Peter.DeRijk@gmail.com>
2013-11-12 15:12:05 +0000

    added tcl::mathfunc::codingcat

---
+++ libext/expr_gene.tcl
@@ -0,0 +1,13 @@
+proc tcl::mathfunc::codingcat {args} {
+	if {[regexp CDS $args]} {
+		return C
+	} elseif {[regexp UTR $args]} {
+		return U
+	} elseif {[regexp RNA $args]} {
+		return R
+	} elseif {[regexp splice $args]} {
+		return s
+	} else {
+		return -
+	}
+}
