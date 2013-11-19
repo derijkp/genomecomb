34bcfedaead4a5069d3f585bc1c0cbde86dfb9d2
Peter.DeRijk <Peter.DeRijk@gmail.com>
2013-11-19 02:21:12 +0000

    cg_index fix: had introduced error with getting size

--- lib/cg_index.tcl
+++ lib/cg_index.tcl
@@ -70,7 +70,9 @@ proc cg_index {args} {
 		set f [gzopen $file]
 		set header [tsv_open $f]
 		catch {close $f}
-		set size [cg_size $bcol]
+		set bcol [bcol_open $indexfile]
+		set size [bcol_size $bcol]
+		bcol_close $bcol
 		dict set info file $file
 		dict set info lineindexfile [file tail $indexfile]
 		dict set info header $header
