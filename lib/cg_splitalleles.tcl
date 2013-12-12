3ce2a85a5a7a9d6d302f01977ef96f2b074b22fb
Peter.DeRijk <Peter.DeRijk@gmail.com>
2013-12-12 09:46:58 +0000

    added cg_splitalleles (wip)

---
+++ lib/cg_splitalleles.tcl
@@ -0,0 +1,92 @@
+proc cg_splitalleles {args} {
+#	set pos 0
+#	while 1 {
+#		set key [lindex $args $pos]
+#		switch -- $key {
+#			-o {
+#				incr pos
+#				set outdir [file_absolute [lindex $args $pos]]
+#				incr pos
+#			}
+#			-- break
+#			default {
+#				break
+#			}
+#		}
+#	}
+#	set args [lrange $args $pos end]
+	if {[llength $args] != 1} {
+		error "format is: cg splitalleles file"
+		exit 1
+	}
+	foreach file $args break
+	set f [gzopen $file]
+	set header [tsv_open $f comment]
+	set poss [tsv_basicfields $header]
+	set apos [lindex $poss 5]
+	set rpos [lindex $poss 4]
+	set samples [samples $header]
+	set sposs {}
+	foreach sample $samples {
+		lappend sposs {*}[list_cor $header [list alleleSeq1-$sample alleleSeq2-$sample sequenced-$sample]]
+	}
+	puts $comment
+	puts [join $header \t]
+	while {![eof $f]} {
+		set line [split [gets $f] \t]
+		if {![llength $line]} continue
+		set ref [lindex $line $rpos]
+		set alleles [split [lindex $line $apos] ,]
+		if {[llength $alleles] == 1} {
+			set a($alleles) $line
+		} else {
+			unset -nocomplain a
+			foreach vs $line {
+				set vs [split $vs ,]
+				if {[llength $vs] == 1} {
+					foreach allele $alleles {
+						lappend a($allele) $vs
+					}
+				} else {
+					foreach allele $alleles v $vs {
+						lappend a($allele) $v
+					}
+				}
+			}
+		}
+		foreach alt [ssort -natural $alleles] {
+			set line $a($alt)
+			foreach {a1pos a2pos seqpos} $sposs {
+				set a1 [lindex $line $a1pos]
+				set a2 [lindex $line $a2pos]
+				set seq [lindex $line $seqpos]
+				if {$seq eq "u"} {
+					set seq u
+				} elseif {$a1 eq $alt} {
+					if {$a2 eq $alt} {
+						set seq m
+					} elseif {$a2 eq $ref} {
+						set seq t
+					} else {
+						set seq c
+					}
+				} elseif {$a2 eq $alt} {
+					if {$a2 eq $ref} {
+						set seq t
+					} else {
+						set seq c
+					}
+				} elseif {$a1 eq $ref && $a2 eq $ref} {
+					set seq r
+				} else {
+					set seq o
+				}
+				lset line $seqpos $seq
+			} 
+		} 
+		foreach allele [ssort -natural $alleles] {
+			puts [join $a($allele) \t]
+		}
+	}
+	close $f
+}
