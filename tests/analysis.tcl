e6880e65f76b508f3b05e38f006de49d7d9230fd
Peter.DeRijk <Peter.DeRijk@gmail.com>
2014-01-06 02:26:25 +0000

    test fix: exportplink names with -

--- tests/analysis.tcl
+++ tests/analysis.tcl
@@ -12,12 +12,13 @@ test exportplink {basic} {
 } {}
 
 test exportplink {names with -} {
-	file delete {*}[glob tmp/temp*]
+	catch {file delete {*}[glob tmp/temp*]}
 	set header [cg select -h data/vars3.sft]
 	set header [string_change $header {-sample1 -m1-sample1 -sample2 -m2-sample1}]
 	cg select -nh $header data/vars3.sft tmp/tempsrc.tsv
 	set c [file_read data/expected-vars3.tfam.pre]
 	set c [string_change $c {sample1 m1-sample1 sample2 m2-sample1}]
+	file_write tmp/expected-temp.tfam.pre $c
 	exec cg exportplink tmp/tempsrc.tsv tmp/temp
 	exec diff tmp/temp.tfam.pre tmp/expected-temp.tfam.pre
 	exec diff tmp/temp.tped data/expected-vars3.tped
