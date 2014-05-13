88c5533beb536f75072e199966d05e156a2cfc44
Peter.DeRijk <Peter.DeRijk@gmail.com>
2014-05-13 10:15:23 +0000

    cg regselect: support compressed file

--- lib/regions.tcl
+++ lib/regions.tcl
@@ -173,18 +173,19 @@ proc regselect {regfile1 regfile2 {near -1}} {
 	global cache
 	# catch {close $f1}
 	# catch {close $f2}
-	set f1 [open $regfile1]
+	set f1 [gzopen $regfile1]
 	set poss1 [open_region $f1]
-	close $f1
+	gzclose $f1
 	if {$regfile2 ne ""} {
-		set f2 [open $regfile2]
+		set f2 [gzopen $regfile2]
 		set poss2 [open_region $f2]
-		close $f2
+		gzclose $f2
 	} else {
 		set poss2 {0 0 0}
 	}
 	# puts [list reg_join $regfile1 {*}$poss1 $regfile2 {*}$poss2]
-	exec reg_select $regfile1 {*}$poss1 $regfile2 {*}$poss2 $near >@ stdout 2>@ stderr
+	set cat [gzcat $regfile1]
+	exec {*}$cat $regfile1 | reg_select {*}$poss1 $regfile2 {*}$poss2 $near >@ stdout 2>@ stderr
 }
 
 proc cg_regselect {args} {
--- src/reg_select.c
+++ src/reg_select.c
@@ -19,33 +19,27 @@ int main(int argc, char *argv[]) {
 	DStringArray *result1=NULL,*result2=NULL,*resultkeep=NULL,*resulttemp=NULL;
 	DString *line1 = NULL,*line2 = NULL,*linekeep = NULL,*linetemp = NULL,*empty=NULL;
 	DString *chromosome1 = NULL,*chromosome2 = NULL,*curchromosome = NULL,*chromosomekeep = NULL;
-	off_t fpos;
-	uint64_t progress = 20000000L;
 	int comp,chr1pos,start1pos,end1pos,chr2pos,start2pos,end2pos,max1,max2;
 	unsigned int numfields1,numfields2,numfields,pos1,pos2;
 	int endkeep=-1,near,near2;
 	int start1,end1,start2,end2;
 	int prevstart1 = -1,prevend1 = -1,prevstart2 = -1,prevend2 = -1;
 	int error2,datanear=-1;
-	if ((argc != 10)) {
-		fprintf(stderr,"Format is: reg_select file1 chrpos1 startpos1 endpos1 file2 chrpos2 startpos2 endpos2 datanear");
+	if ((argc != 9)) {
+		fprintf(stderr,"Format is: reg_select chrpos1 startpos1 endpos1 file2 chrpos2 startpos2 endpos2 datanear");
 		exit(EXIT_FAILURE);
 	}
-	f1 = fopen64_or_die(argv[1],"r");
-	chr1pos = atoi(argv[2]);
-	start1pos = atoi(argv[3]);
-	end1pos = atoi(argv[4]);
+	f1 = stdin;
+	chr1pos = atoi(argv[1]);
+	start1pos = atoi(argv[2]);
+	end1pos = atoi(argv[3]);
 	max1 = chr1pos ; if (start1pos > max1) {max1 = start1pos;} ; if (end1pos > max1) {max1 = end1pos;} ;
-	if (strlen(argv[5]) == 1 && argv[5][0] == '-') {
-		f2 = stdin;
-	} else {
-		f2 = fopen64_or_die(argv[5],"r");
-	}
-	chr2pos = atoi(argv[6]);
-	start2pos = atoi(argv[7]);
-	end2pos = atoi(argv[8]);
-	datanear = atoi(argv[9]);
-NODPRINT("reg_select %s %d %d %d %s %d %d %d %d",argv[1],chr1pos,start1pos,end1pos,argv[5],chr2pos,start2pos,end2pos,datanear)
+	f2 = fopen64_or_die(argv[4],"r");
+	chr2pos = atoi(argv[5]);
+	start2pos = atoi(argv[6]);
+	end2pos = atoi(argv[7]);
+	datanear = atoi(argv[8]);
+NODPRINT("reg_select %d %d %d %s %d %d %d %d",chr1pos,start1pos,end1pos,argv[4],chr2pos,start2pos,end2pos,datanear)
 	max2 = chr2pos ; if (start2pos > max2) {max2 = start2pos;} ; if (end2pos > max2) {max2 = end2pos;} ;
 	/* allocate */
 	line1 = DStringNew(); line2=DStringNew(); linekeep=DStringNew(); empty = DStringNew();
@@ -59,26 +53,21 @@ NODPRINT("reg_select %s %d %d %d %s %d %d %d %d",argv[1],chr1pos,start1pos,end1p
 	skip_header(f2,line2,&numfields2,&pos2);
 	error2 = DStringGetTab(line2,f2,max2,result2,1,&numfields);	pos2++;
 	if (!error2) {
-		check_numfieldserror(numfields,numfields2,line2,argv[5],&pos2);
+		check_numfieldserror(numfields,numfields2,line2,argv[4],&pos2);
 		chromosome2 = result2->data+chr2pos;
 		sscanf(result2->data[start2pos].string,"%d",&start2);
 		sscanf(result2->data[end2pos].string,"%d",&end2);
 	}
 	while (!DStringGetTab(line1,f1,max1,result1,0,&numfields)) {
 		pos1++;
-		check_numfieldserror(numfields,numfields1,line1,argv[1],&pos1);
+		check_numfieldserror(numfields,numfields1,line1,"stdin",&pos1);
 		chromosome1 = result1->data+chr1pos;
 		sscanf(result1->data[start1pos].string,"%d",&start1);
 		sscanf(result1->data[end1pos].string,"%d",&end1);
 NODPRINT("%d\t%s\t%d\t%d",1,Loc_ChrString(chromosome1),start1,end1)
 NODPRINT("%d\t%s\t%d\t%d",2,Loc_ChrString(chromosome2),start2,end2)
 NODPRINT("%d\t%s\t%d\t%d",2,Loc_ChrString(curchromosome),start2,end2)
-		checksortreg(curchromosome,&prevstart1,&prevend1,chromosome1,start1,end1,argv[1]);
-		fpos = ftello(f1);
-		if (fpos > progress) {
-			fprintf(stderr,"filepos: %llu\n",(unsigned long long)fpos);
-			progress += 20000000L;
-		}
+		checksortreg(curchromosome,&prevstart1,&prevend1,chromosome1,start1,end1,"stdin");
 		comp = DStringLocCompare(chromosome2, chromosome1);
 		while (!error2 && ((comp < 0) || ((comp == 0) && (end2 <= start1)))) {
 			/* keep data of previous */
@@ -97,7 +86,7 @@ NODPRINT("%d\t%s\t%d\t%d",2,Loc_ChrString(curchromosome),start2,end2)
 				comp = -1;
 				break;
 			} else {
-				check_numfieldserror(numfields,numfields2,line2,argv[5],&pos2);
+				check_numfieldserror(numfields,numfields2,line2,argv[4],&pos2);
 			}
 			chromosome2 = result2->data+chr2pos;
 			sscanf(result2->data[start2pos].string,"%d",&start2);
