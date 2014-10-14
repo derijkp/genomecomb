c7e358e263074e7d60e38fff2627107c35b0eb46
Peter.DeRijk <Peter.DeRijk@gmail.com>
2014-10-14 10:19:57 +0000

    moved cg_multicomparfast.tcl to cg_multicompar.tcl

--- lib/cg_multicompar.tcl
+++ lib/cg_multicompar.tcl
@@ -4,280 +4,53 @@
 # this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
 
-proc multicompar_annot_join {cur1 cur2} {
-	global joinposs1 mergeposs1 joinposs2 mergeposs2 dummy1 dummy2 restposs1 restposs2 refpos1 refpos2 altpos1 altpos2 alleleposs1 alleleposs2 listfields1 listfields2 sequenced2pos
-	if {[inlist {{} -} $cur1]} {
-		set region [list_sub $cur2 $joinposs2]
-		set merge [list_sub $cur2 $mergeposs2]
-		if {$cur1 eq "-"} {
-			set cur1 $dummy1
-		} else {
-			set cur1 [list_change $dummy1 {- {}}]
+proc multi_merge_job {varsfile files {split 1}} {
+	global multi_merge_num
+	upvar job_logdir job_logdir
+	set newfiles {}
+	if {[llength $files] < 2} {
+		mklink [lindex $files 0] $varsfile
+		return
+	} 
+	foreach {file1 file2} $files {
+		if {$file2 eq ""} continue
+		incr multi_merge_num
+		job multi_merge-$multi_merge_num -deps [list $file1 $file2] -vars split -targets $varsfile.$multi_merge_num -code {
+			set f [gzopen $dep1]
+			set header [tsv_open $f]
+			set poss1 [tsv_basicfields $header 6 $dep1]
+			close $f
+			set f [gzopen $dep2]
+			set header [tsv_open $f]
+			set poss2 [tsv_basicfields $header 6 $dep2]
+			close $f
+			exec multi_merge $dep1 {*}$poss1 $dep2 {*}$poss2 $split > $target.temp
+			file rename -force $target.temp $target
 		}
-		if {$sequenced2pos != -1} {
-			set sequenced [lindex $cur2 $sequenced2pos]
-		} else {
-			set sequenced v
-		}
-	} elseif {[inlist {{} -} $cur2]} {
-		set region [list_sub $cur1 $joinposs1]
-		set merge [list_sub $cur1 $mergeposs1]
-		if {$cur2 eq "-"} {
-			set cur2 $dummy2
-		} else {
-			set cur2 [list_change $dummy2 {- {}}]
-		}
-		set sequenced ?
+		lappend newfiles $varsfile.$multi_merge_num
+	}
+	if {$file2 eq ""} {
+		lappend newfiles $file1
+	}
+	if {[llength $newfiles] > 1} {
+		multi_merge_job $varsfile $newfiles $split
 	} else {
-		set region [list_sub $cur1 $joinposs1]
-		set merge {}
-		foreach el1 [list_sub $cur1 $mergeposs1] el2 [list_sub $cur2 $mergeposs2] {
-			lappend merge [list_union $el1 $el2]
-		}
-		if {$sequenced2pos != -1} {
-			set sequenced [lindex $cur2 $sequenced2pos]
-		} else {
-			set sequenced v
+		incr multi_merge_num
+		job multi_merge-$multi_merge_num -deps $newfiles -vars split -targets $varsfile -code {
+			file rename -force $dep1 $target
 		}
 	}
-	if {$refpos1 == -1} {
-		if {$refpos2 == -1} {
-			set ref ?
-		} else {
-			set ref [lindex $cur2 $refpos2]
-		}
-	} else {
-		set ref [lindex $cur1 $refpos1]
-		if {$ref eq "?"} {
-			if {$refpos2 != -1} {set ref [lindex $cur2 $refpos2]}
-		}
-	}
-	# merge alt
-	if {$altpos1 == -1} {
-		set alt1 [list_remove [list_remdup [list_sub $cur1 $alleleposs1]] $ref]
-	} else {
-		set alt1 [split [lindex $cur1 $altpos1] ,]
-		if {[inlist ? $alt1]} {set alt1 ""}
-	}
-	if {$altpos2 == -1} {
-		set alt2 [list_remove [list_remdup [list_sub $cur2 $alleleposs2]] $ref]
-	} else {
-		set alt2 [split [lindex $cur2 $altpos2] ,]
-		if {[inlist ? $alt2]} {set alt2 ""}
-	}
-	# putsvars cur1 cur2 alt1 alt2
-	if {$alt1 eq $alt2} {
-		set alt $alt1
-	} elseif {![llength $alt1]} {
-		set alt $alt2
-	} elseif {![llength $alt2]} {
-		set alt $alt1
-	} else {
-		set alt [list_union $alt1 $alt2]
-		set a1poss [list_cor $alt1 $alt]
-		set a2poss [list_cor $alt2 $alt]
-		foreach p $listfields1 {
-			set v [lindex $cur1 $p]
-			set v [split $v ,]
-			set v [list_sub $v $a1poss]
-			set v [list_change $v {{} -}]
-			lset cur1 $p [join $v ,]
-		}
-		foreach p $listfields2 {
-			set v [lindex $cur2 $p]
-			set v [split $v ,]
-			set v [list_sub $v $a2poss]
-			set v [list_change $v {{} -}]
-			lset cur2 $p [join $v ,]
-		}
-	}
-	set remove [list $ref ? - N @]
-	set type [lindex $region 3]
-	if {$type eq "snp"} {
-		lappend remove {}
-	}
-	set alt [list_lremove $alt $remove]
-	set result $region
-	lappend result $ref
-	lappend result [join $alt ,]
-	lappend result {*}[list_sub $cur1 $restposs1]
-	lappend result $sequenced
-	lappend result {*}[list_sub $cur2 $restposs2]
-	lappend result {*}$merge
-	return [join $result \t]
+	
 }
 
-proc multicompar_getcomp {line poss split file prevcomp1} {
-	set comp1 [list_sub $line $poss]
-	if {![llength $line]} {return ~}
-	if {$split && [regexp , [lindex $comp1 end]]} {
-		error "split mode does not allow multiallelic variants: $comp1 in file $file"
-	}
-	set comp1 [join $comp1 " "]
-	set prevcheck [loc_compare $prevcomp1 $comp1]
-	if {$prevcheck >= 0} {
-		if {$prevcheck > 0} {
-			error "sorting error in \"$file\": \"$prevcomp1\" comes before \"$comp1\""
-		} elseif {$prevcheck == 0 && !$split} {
-			error "error in \"$file\": file uses split alleles (\"$prevcomp1\" occurs more than once and you are not running multicompar with the -split option)"
-		}
-	}
-	return $comp1
-}
-
-proc multicompar {compar_file dir {split 0} {listfields {}}} {
-	global cache joinposs1 joinposs2 comparposs1 mergeposs1 comparposs2 mergeposs2 dummy1 dummy2 restposs1 restposs2 refpos1 refpos2 altpos1 altpos2 alleleposs1 alleleposs2 listfields1 listfields2 sequenced2pos
-	catch {close $f1}; catch {close $f2}; catch {close $o}
-	set mergefields {xRef geneId mrnaAcc proteinAcc symbol orientation component componentIndex hasCodingRegion impact nucleotidePos proteinPos annotationRefSequence sampleSequence genomeRefSequence pfam}
-	set allelefields {alleleSeq1 alleleSeq2}
-	#
-	if {[file isdir $dir]} {
-		set name [file tail $dir]
-		set file2 [gzfile $dir/fannotvar-$name.tsv]
-	} else {
-		set base [file root [file tail [gzroot $dir]]]
-		set name [sourcename $base]
-		set file2 [gzfile $dir]
-	}
-	if {![file exists $compar_file]} {
-		file_write $compar_file [join {chromosome begin end type ref alt} \t]
-	}
-	set f1 [gzopen $compar_file]
-	set header1 [tsv_open $f1]
-	if {[inlist $header1 alleleSeq1-$name]} {
-		error "$name already present in $compar_file"
-	}
-	set comparposs1 [tsv_basicfields $header1 6 0]
-	if {([lsearch $comparposs1 -1] != -1)} {
-		puts stderr "header error in comparfile $compar_file"
-		exit 1
-	}
-	set refpos1 [lindex $comparposs1 4]
-	set altpos1 [lindex $comparposs1 5]
-	if {$altpos1 == -1} {
-		set alleleposs1 [list_find -glob $header1 alleleSeq*]
-	}
-	set joinposs1 [lrange $comparposs1 0 3]
-	if {$split} {
-		set comparposs1 [list_sub $comparposs1 {0 1 2 3 5}]
-	} else {
-		set comparposs1 $joinposs1
-	}
-	set tp1 [lindex $comparposs1 0]
-	set dummy1 [list_fill [llength $header1] ?]
-	set f2 [gzopen $file2]
-	set header2 [tsv_open $f2]
-	set comparposs2 [tsv_basicfields $header2 6 0]
-	if {([lsearch [lrange $comparposs2 0 3] -1] != -1)} {
-		puts stderr "header error in fannot_varfile2 $compar_file"
-		exit 1
-	}
-	set refpos2 [lindex $comparposs2 4]
-	set altpos2 [lindex $comparposs2 5]
-	if {$altpos2 == -1} {
-		set alleleposs2 [list_find -glob $header2 alleleSeq*]
-	}
-	set joinposs2 [lrange $comparposs2 0 3]
-	if {$split} {
-		set comparposs2 [list_sub $comparposs2 {0 1 2 3 5}]
-	} else {
-		set comparposs2 $joinposs2
-	}
-	set tp2 [lindex $comparposs2 0]
-	set mergefields [list_common $mergefields $header2]
-	set nonmergefields [list_lremove $header2 $mergefields]
-	set mergeposs1 [list_cor $header1 $mergefields]
-	set mergeposs2 [list_cor $header2 $mergefields]
-	set dummy2 [list_fill [llength $header2] ?]
-	set listfields1 [list_find -glob $header1 l:*]
-	set listfields2 [list_find -glob $header2 l:*]
-	foreach p $listfields {
-		set poss [list_find -glob $header1 $p]
-		if {[llength $poss]} {lappend listfields1 {*}$poss}
-		set poss [list_find -glob $header2 $p]
-		if {[llength $poss]} {lappend listfields2 {*}$poss}
-	}
-	set listfields1 [lsort -integer [list_remdup $listfields1]]
-	set listfields2 [lsort -integer [list_remdup $listfields2]]
-	# make output header
-	set restfields1 [list_lremove [list_sub $header1 -exclude $comparposs1] $mergefields]
-	set restfields1 [list_remove $restfields1 ref reference alt]
-	set restposs1 [list_cor $header1 $restfields1]
-	set oheader [list_concat {chromosome begin end type ref alt} $restfields1]
-	set restfields2 [list_lremove [list_sub $header2 -exclude $comparposs2] $mergefields]
-	set sequenced2pos [lsearch $header2 sequenced]
-	set restfields2 [list_remove $restfields2 ref reference alt sequenced]
-	set restposs2 [list_cor $header2 $restfields2]
-	lappend oheader sequenced-$name
-	foreach field $restfields2 {
-		lappend oheader ${field}-$name
-	}
-	set oheader [list_concat $oheader $mergefields]
-	# start
-	set prevcomp1 {}
-	set prevcomp2 {}
-	set o [open $compar_file.temp w]
-	# puts $o "#split: $split"
-	puts $o [join $oheader \t]
-	set cur1 [split [gets $f1] \t]
-	if {[llength $cur1]} {lset cur1 $tp1 [chr_clip [lindex $cur1 $tp1]]}
-	set comp1 [multicompar_getcomp $cur1 $comparposs1 $split $compar_file $prevcomp1]
-	set cur2 [split [gets $f2] \t]
-	if {[llength $cur2]} {lset cur2 $tp2 [chr_clip [lindex $cur2 $tp2]]}
-	set comp2 [multicompar_getcomp $cur2 $comparposs2 $split $file2 $prevcomp2]
-	set num 1; set next 100000
-	while {![eof $f1] || ![eof $f2]} {
-		incr num
-		if {$num > $next} {putslog $num ; incr next 100000}
-		set d [loc_compare $comp1 $comp2]
-		if {$d == 0} {
-			puts $o [multicompar_annot_join $cur1 $cur2]
-			set cur1 [compare_annot_getline $f1]
-			if {[llength $cur1]} {lset cur1 $tp1 [chr_clip [lindex $cur1 $tp1]]}
-			set comp1 [multicompar_getcomp $cur1 $comparposs1 $split $compar_file $prevcomp1]
-			set prevcomp1 $comp1
-			set cur2 [compare_annot_getline $f2]
-			if {[llength $cur2]} {lset cur2 $tp2 [chr_clip [lindex $cur2 $tp2]]}
-			set comp2 [multicompar_getcomp $cur2 $comparposs2 $split $file2 $prevcomp2]
-			set prevcomp2 $comp2
-		} elseif {$d < 0} {
-			while {$d < 0} {
-				puts $o [multicompar_annot_join $cur1 -]
-				if {[eof $f1]} break
-				set cur1 [compare_annot_getline $f1]
-				if {[llength $cur1]} {lset cur1 $tp1 [chr_clip [lindex $cur1 $tp1]]}
-				set comp1 [multicompar_getcomp $cur1 $comparposs1 $split $compar_file $prevcomp1]
-				set prevcomp1 $comp1
-				if {![llength $cur1]} break
-				incr num
-				if {![expr {$num % 100000}]} {putslog $num}
-				set d [loc_compare $comp1 $comp2]
-			}
-		} else {
-			while {$d > 0} {
-				puts $o [multicompar_annot_join - $cur2]
-				if {[eof $f2]} break
-				set cur2 [compare_annot_getline $f2]
-				if {[llength $cur2]} {lset cur2 $tp2 [chr_clip [lindex $cur2 $tp2]]}
-				set comp2 [multicompar_getcomp $cur2 $comparposs2 $split $file2 $prevcomp2]
-				set prevcomp2 $comp2
-				incr num
-				if {![expr {$num % 100000}]} {putslog $num}
-				set d [loc_compare $comp1 $comp2]
-			}
-		}
-	}
-
-	close $f1; close $f2; close $o
-	catch {file rename -force $compar_file $compar_file.old}
-	file rename -force $compar_file.temp $compar_file
-}
-
-proc cg_multicompar.old {args} {
+proc cg_multicompar {args} {
+	set args [job_init -silent {*}$args]
 	set reannot 0
 	set regonly 0
 	set split 0
 	set listfields {}
+	set targetsfile {}
+	set targetsfield {}
 	set pos 0
 	while 1 {
 		set key [lindex $args $pos]
@@ -299,8 +72,14 @@ proc cg_multicompar.old {args} {
 				incr pos
 				set listfields [lindex $args $pos]
 			}
+			-targetsfile {
+				incr pos
+				set targetsfile [lindex $args $pos]
+				set targetsfield [lindex [split [file root [file tail $targetsfile]] -] end]
+			}
 			-- break
 			default {
+				if {[string index $key 0] eq "-"} {error "unknown option \"$key\""}
 				break
 			}
 		}
@@ -313,35 +92,151 @@ proc cg_multicompar.old {args} {
 		exit 1
 	}
 	foreach {compar_file} $args break
-	set dirs [lrange $args 1 end]
-	foreach dir $dirs {
-		set dir [file_absolute $dir]
-		if {![file isdir $dir] && [llength [cg select -n $dir]]} {
-			set header [cg select -h $dir]
-			set samples [samples $header]
-			set basicfields [list_sub $header -exclude [list_find -regexp $header -]]
-			putslog "$dir is already a multicompar, merging"
-			foreach sample $samples {
-				putslog "Adding $dir sample $sample"
-				catch {file delete $dir.temp}
-				file mkdir $dir.temp
-				set fields $basicfields
-				foreach field [list_sub $header [list_find -regexp $header -$sample]] {
-					set nfield [join [lrange [split $field -] 0 end-1] -]
-					lappend fields $nfield=\$\{$field\}
-				}
-				cg select -f $fields $dir $dir.temp/vars-$sample.tsv
-				multicompar $compar_file $dir.temp/vars-$sample.tsv $split $listfields
-				file delete $dir.temp/vars-$sample.tsv
-			}
-			file delete -force $dir.temp
-		} else {
-			putslog "Adding $dir"
-			multicompar $compar_file $dir $split $listfields
+	unset -nocomplain a
+	if {[file exists $compar_file]} {
+		foreach name [cg select -n $compar_file] {
+			set a($name) $compar_file
 		}
 	}
-	if {$reannot} {
-		putslog "Reannotating $compar_file"
-		multicompar_reannot $compar_file 0 $regonly
+	file mkdir $compar_file.index
+	set workdir $compar_file.index/multicompar
+	# should take into account existing instead of deleting and starting all over -> not now
+	if {[file exists $workdir]} {file delete -force $workdir}
+	file mkdir $workdir
+	set dirs [lrange $args 1 end]
+	set files {}
+	foreach dir $dirs {
+		set dir [file_absolute $dir]
+		if {[file isdir $dir]} {
+			# a directory is given, look for the variant file(s)
+			set name [file tail $dir]
+			if {[file exists $dir/fannotvar-$name.tsv]} {
+				set file [gzfile $dir/fannotvar-$name.tsv]
+				lappend files $file
+				if {[info exists a($name)]} {
+					error "sample \"$name\" in \"$file\" was already in \"$a($name)\""
+				}
+				set a($name) $file
+			} else {
+				foreach file [gzfiles $dir/var-*-$name.tsv] {
+					set base [file root [file tail [gzroot $file]]]
+					set file [gzfile $file]
+					set name [sourcename $base]
+					lappend files $file
+					if {[info exists a($name)]} {
+						error "sample \"$name\" in \"$file\" was already in \"$a($name)\""
+					}
+					set a($name) $file
+				}
+			}
+		} elseif {[llength [cg select -n $dir]]} {
+			set samples [cg select -n $dir]
+			set file $dir
+			lappend files $file
+			foreach name $samples {
+				if {[info exists a($name)]} {
+					error "sample \"$name\" in \"$file\" was already in \"$a($name)\""
+				}
+				set a($name) $file
+			}
+		} else {
+			set base [file root [file tail [gzroot $dir]]]
+			set name [sourcename $base]
+			set file [gzfile $dir]
+			lappend files $file
+			if {[info exists a($name)]} {
+				error "sample \"$name\" in \"$file\" was already in \"$a($name)\""
+			}
+			set a($name) $file
+		}
 	}
+	job_logdir $workdir/log_jobs
+	# merge variants
+	# todo: check for concurrency
+	set multi_merge_num 0
+	if {[file exists $compar_file]} {
+		file rename -force $compar_file $compar_file.old
+		set allfiles [list_concat $compar_file.old $files]
+	} else {
+		set allfiles $files
+	}
+	# for calculating the varlines needed, we can treat targetsfile as just another variant file
+	set files $allfiles
+	if {$targetsfile ne ""} {lappend files $targetsfile}
+	multi_merge_job $workdir/vars.tsv $files $split
+	set todofile $workdir/multitodo.txt
+	set ftodo [open $todofile w]
+	# line with: filename chrpos beginpos endpos typepos refpos altpos todomax todoseqpos
+	puts $ftodo [join [list $workdir/vars.tsv 0 1 2 3 4 5 5 0] \t]
+	# line with: data positioons that must go in the multicompar
+	puts $ftodo ""
+	set reannotheader {chromosome begin end type ref alt}
+	foreach file $allfiles {
+		set f [gzopen $file]
+		set header [tsv_open $f]
+		close $f
+		set file [file_absolute $file]
+		set dir [file dir $file]
+		set filebase [file root [file tail [gzroot $file]]]
+		set basicposs [tsv_basicfields $header 6 $file]
+		set samples [samples $header]
+		if {[llength $samples] > 0} {
+			set keepposs [list_find -glob $header *-*]
+			# in a multicompar file, sequenced columns should already be present
+			# put a value not -1 or -2, so no new one will be added
+			set seqpos 0
+			lappend reannotheader {*}[list_sub $header $keepposs]
+		} else {
+			set sample [sourcename $filebase]
+			set seqpos [lsearch $header sequenced]
+			set keepfields [list_sub $header -exclude $basicposs]
+			if {[string match fannotvar-* [file tail $file]]} {
+				set keepfields [list_lremove $keepfields $mergefields]
+			}
+			set keepfields [list_remove $keepfields $targetsfield]
+			set keepposs [list_cor $header $keepfields]
+			if {$seqpos == -1} {
+				lappend reannotheader sequenced-$sample
+			}
+			foreach field $keepfields {
+				lappend reannotheader ${field}-$sample
+			}
+		}
+		set max [lmath_max [list_concat $keepposs $basicposs]]
+		# line with: filename chrpos beginpos endpos typepos refpos altpos maxcoltoread todoseqpos sizeofnextline
+		puts $ftodo [join [list $file {*}$basicposs $max $seqpos [llength $keepposs]] \t]
+		# line with: data positioons that must go in the multicompar
+		puts $ftodo [join $keepposs \t]
+	}
+	set len [expr {[llength $allfiles]+1}]
+	if {$targetsfile ne ""} {
+		set f [open $targetsfile]
+		set header [tsv_open $f]
+		close $f
+		lappend reannotheader $targetsfield
+		set file [file_absolute $targetsfile]
+		set basicposs [tsv_basicfields $header $file]
+		# if seqpos != -1, no new calculated sequence column will be added
+		# -2 is used to indicate to put an empty field instead of a ? for no match
+		set seqpos -2
+		set keepposs [lsearch $header name]
+		set max [lmath_max [list_concat $keepposs $basicposs]]
+		puts $ftodo [join [list $file {*}$basicposs $max $seqpos [llength $keepposs]] \t]
+		puts $ftodo [join $keepposs \t]
+		incr len
+	}
+	close $ftodo
+	job multi_join -deps [list $todofile] -vars {split len reannotheader} -targets $compar_file -code {
+		file_write $target.temp [join $reannotheader \t]\n
+		exec multi_join $dep1 $len $split >> $target.temp
+		file rename -force $target.temp $target
+	}
+	if {$reannot} {
+		job multicompar_reannot -deps {$compar_file} -vars {regonly} -targets {$compar_file $compar_file.reannot} -code {
+			putslog "Reannotating $dep"
+			multicompar_reannot $dep 0 $regonly
+			file_write $dep.reannot [timestamp]
+		}
+	}
+	job_wait
 }
