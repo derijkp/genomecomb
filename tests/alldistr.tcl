aedc3b90f221ed6e42eac536c8e6bccb652f95ee
Peter.DeRijk <Peter.DeRijk@gmail.com>
2015-04-10 11:39:59 +0000

    added tests for distributions

---
+++ tests/alldistr.tcl
@@ -0,0 +1,16 @@
+#!/bin/sh
+# the next line restarts using wish \
+exec tclsh "$0" "$@"
+
+set version 0.10.0
+
+set keeppath $::env(PATH)
+set testlist [list /home/peter/build/genomecomb-$version-Linux-i686 /home/peter/build/genomecomb-$version-Linux-x86_64]
+set cgversion [lindex $testlist 0]
+
+foreach cgversion $testlist {
+	puts "\n\n==================== Testing $cgversion ===================="
+	set ::env(PATH) $cgversion:$::keeppath
+	# source annot.tcl
+	source all.tcl
+}
