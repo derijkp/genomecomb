b3572a9184c38aee3af87aad8f8968ef9c3f7114
Peter.DeRijk <Peter.DeRijk@gmail.com>
2015-04-30 10:31:37 +0000

    added cg homwes and cg homwes_compare

---
+++ lib/cg_homwes.tcl
@@ -0,0 +1,253 @@
+#!/bin/sh
+# the next line restarts using wish \
+exec cg source "$0" ${1+"$@"}
+
+proc findfield {fields pattern} {
+	lindex $fields [lsearch -glob $fields $pattern]
+}
+
+proc cg_homwes {args} {
+	set callers {gatk-rdsbwa- sam-rdsbwa-}
+	set allowedheterozygous 1
+	set homozygdensity 200
+	set homozyggap 4000
+	set homozygwindowsnp 20
+	set snpsonly 0
+	set dbdir {}
+	set vcf 0
+	set pos 0
+	set variantsonly 0
+	foreach {key value} $args {
+		switch -- $key {
+			-dbdir {
+				set dbdir $value
+			}
+			-callers {
+				set callers $value
+			}
+			-allowedheterozygous -- -htz {
+				set allowedheterozygous $value
+			}
+			-density {
+				set homozygdensity $value
+			}
+			-gap {
+				set homozyggap $value
+			}
+			-window {
+				set homozygwindowsnp $value
+			}
+			-variantsonly {
+				set variantsonly $value
+			}
+			-snpsonly {
+				set snpsonly $value
+			}
+			default break
+		}
+		incr pos 2
+	}
+	set args [lrange $args $pos end]
+	set len [llength $args]
+	if {$len < 2 || $len > 3} {
+		errorformat homwes
+		exit 1
+	}
+	set resultfile {}
+	foreach {annotcomparfile samples resultfile} $args break
+	set annotcomparfile [file normalize $annotcomparfile]
+	if {$resultfile eq ""} {
+		set resultfile [file root $annotcomparfile]-homwes.tsv
+	}
+	set resultfile [file normalize $resultfile]
+	set resultfilebase [file root $resultfile]
+	set workdir $resultfilebase.work
+	file mkdir $workdir
+	set workbase $workdir/[file tail $resultfilebase]
+	set dir [file dir $annotcomparfile]
+	set tail [file tail $annotcomparfile]
+	if {[file ext $annotcomparfile] eq ".vcf"} {
+		if {$dbdir eq ""} {
+			puts stderr "No -dbdir option given, this is needed for analysing"
+			exit 1
+		}
+		set vcf 1
+		set tsvfile $workdir/[file root [file tail $annotcomparfile]].tsv
+		putslog "Converting vcf file $annotcomparfile to tsv $tsvfile"
+		cg vcf2tsv $annotcomparfile $tsvfile.temp
+		putslog "annotating $tsvfile"
+		cg annotate $tsvfile.temp $tsvfile.temp2 {*}[glob $dbdir/reg_*_microsat.tsv $dbdir/reg_*_simpleRepeat.tsv]
+		file rename -force $tsvfile.temp2 $tsvfile
+		file delete $tsvfile.temp
+		set usefile $tsvfile
+	} elseif {[llength [list_common [cg select -h $annotcomparfile] {microsat simpleRepeat}]] != 2} {
+		if {$dbdir eq ""} {
+			puts stderr "No -dbdir option given, this is needed for annotating the file"
+			exit 1
+		}
+		set tsvfile $workdir/[file root [file tail $annotcomparfile]].tsv
+		putslog "annotating $tsvfile"
+		cg annotate $annotcomparfile $tsvfile.temp2 {*}[glob $dbdir/reg_*_microsat.tsv $dbdir/reg_*_simpleRepeat.tsv]
+		file rename -force $tsvfile.temp2 $tsvfile
+		file delete $tsvfile.temp
+		set usefile $tsvfile
+	} else {
+		set usefile $annotcomparfile
+	}
+	cd $dir
+	putslog "Gathering samples"
+	set samples [list_remdup $samples]
+ 	set header [cg select -h $usefile]
+	set hsamples {}
+	foreach hsample [samples $header] {
+		lappend hsamples [lindex [split $hsample -] end]
+	}
+	set hsamples [list_remdup $hsamples]
+	if {![llength $hsamples]} {
+		putslog "Only one sample in file: not using different variant callers"
+		set hassamples 0
+		set callers {}
+	} else {
+		set hassamples 1
+	}
+	if {![llength $samples]} {
+		if {[llength $hsamples]} {
+			set samples $hsamples
+		} else {
+			set samples sample1
+		}
+	} elseif {[llength $hsamples]} {
+		set notasample [list_lremove $samples $hsamples]
+		if {[llength $notasample]} {
+			puts stderr "error: not a sample: $notasample"
+			exit 1
+		}
+	}
+	set resultfiles {}
+	foreach sample $samples {
+		putslog "Sample $sample"
+		set sworkbase $workbase-$sample
+		set query {}
+		set fields {chromosome begin end type ref alt}
+		set filter {}
+		set caller1 [lindex $callers 0]
+		if {$hassamples} {
+			set postfix -$caller1$sample
+		} else {
+			set postfix ""
+		}
+		set field [findfield $header filter$postfix]
+		if {$field ne ""} {
+			lappend query "\$$field in {. PASS}"
+		} elseif {$postfix ne ""} {
+			set field [findfield $header filter]
+			if {$field ne ""} {
+				lappend query "\$$field in {. PASS}"
+			} else {
+				puts "warning: field \"filter$postfix\" is missing"
+			}
+		} else {
+			puts "warning: field \"filter$postfix\" is missing"
+		}
+		set field [findfield $header genoqual$postfix]
+		if {$field ne ""} {
+			lappend query "def(\$$field,100) > 40"
+		} else {
+			puts "warning: field \"genoqual$postfix\" is missing"
+		}
+		if {$snpsonly} {
+			lappend query {$type eq "snp"}
+		}
+		set dosame {}
+		set callers_used {}
+		foreach caller $callers {
+			if {[findfield $header alleleSeq1-$caller$sample] ne "" && [findfield $header alleleSeq2-$caller$sample] ne ""} {
+				lappend dosame $caller$sample
+				lappend callers_used $caller
+			} else {
+				puts "warning: field \"alleleSeq1-$caller$sample\" or \"alleleSeq2-$caller$sample\" is missing"
+			}
+		}
+		if {[llength $dosame] > 1} {
+			lappend query "same(\"[join $dosame \",\"]\")"
+		}
+		lappend fields "alleleSeq*-$sample=\$alleleSeq*$postfix"
+		if {$variantsonly} {
+			if {"sequenced$postfix" in $header} {
+				lappend query "\$sequenced$postfix eq \"v\""
+			} elseif {"zyg$postfix" in $header} {
+				lappend query "\$zyg$postfix ni \"u r\""
+			} else {
+				lappend query "\$alleleSeq1$postfix != toupper(\$ref) || \$alleleSeq2$postfix != toupper(\$ref)"
+			}
+		} else {
+			if {"sequenced$postfix" in $header} {
+				lappend query "\$sequenced$postfix ne \"u\""
+			} elseif {"zyg$postfix" in $header} {
+				lappend query "\$zyg$postfix ni \"u\""
+			}
+		}
+		if {$query eq ""} {set query 1}
+		putslog "Quality filtering data"
+		cg select \
+			-q "\$microsat==\"\" && \$simpleRepeat==\"\" && (chr_clip(\$chromosome) ni {X Y M MT}) && [join $query &&]" \
+			-f $fields \
+			$usefile ${sworkbase}-filtered.tsv
+		
+		# export to plink
+		putslog "Export to plink"
+		cg exportplink --samples $sample ${sworkbase}-filtered.tsv $sworkbase
+		# 
+		set c [split [string trim [file_read $sworkbase.tfam.pre]] \n]
+		set o [open $sworkbase.tfam w]
+		foreach line $c {
+			lset line 4 1
+			lset line 5 2
+			puts $o [join $line \t]
+		}
+		close $o
+		putslog "Recode"
+		exec plink --tfile $sworkbase --out temp --recode --transpose
+		putslog "Run homozygosity mapping"
+		exec plink --tfile $sworkbase --out $sworkbase --make-bed
+		exec plink --bfile $sworkbase --out $sworkbase --homozyg-window-snp $homozygwindowsnp  --homozyg-window-het $allowedheterozygous --homozyg-window-threshold 0.05 --homozyg-window-missing 10 --homozyg-snp 10  --homozyg-density $homozygdensity  --homozyg-gap $homozyggap --homozyg-group
+		# exec plink --tfile ${sworkbase} --missing
+		
+		putslog "Create result $resultfile"
+		set f [open $sworkbase.hom]
+		set o [open $sworkbase-hom.tsv w]
+		if {!$vcf} {
+			puts $o "# analysed file: $usefile"
+		} else {
+			puts $o "# analysed file: $annotcomparfile (converted to $usefile)"
+		}
+		puts $o "# samples: $samples"
+		puts $o "# callers_used: $callers_used"
+		puts $o "# callers_param: $callers"
+		puts $o "# allowedheterozygous: $allowedheterozygous"
+		puts $o "# variantsonly: $variantsonly"
+		puts $o "# snpsonly: $snpsonly"
+		puts $o "# dbdir: $dbdir"
+		set line [gets $f]
+		if {[list {*}$line] ne "FID IID PHE CHR SNP1 SNP2 POS1 POS2 KB NSNP DENSITY PHOM PHET"} {
+			error "wrong header for $sworkbase.hom (plink hom output changed)"
+		}
+		puts $o [join {chromosome begin end DENSITY PHOM PHET FID IID PHE SNP1 SNP2 KB NSNP} \t]
+		set poss [list_cor {FID IID PHE CHR SNP1 SNP2 POS1 POS2 KB NSNP DENSITY PHOM PHET} {CHR POS1 POS2 DENSITY PHOM PHET FID IID PHE SNP1 SNP2 KB NSNP}]
+		while {[gets $f line] >= 0} {
+			foreach {FID IID PHE CHR SNP1 SNP2 POS1 POS2 KB NSNP DENSITY PHOM PHET} $line break
+			puts $o [join [list_sub $line $poss] \t]
+		}
+		close $o
+		close $f
+		file rename -force $sworkbase-hom.tsv $resultfilebase-$sample.tsv
+		lappend resultfiles $resultfilebase-$sample.tsv
+	}
+	if {[llength $samples] == 1} {
+		file rename -force $resultfilebase-$sample.tsv $resultfile
+	} else {
+		putslog "Making $resultfile"
+		cg multireg $resultfile.temp {*}$resultfiles
+		file rename -force $resultfile.temp $resultfile
+	}
+}
---
+++ lib/cg_homwes_compare.tcl
@@ -0,0 +1,58 @@
+#!/bin/sh
+# the next line restarts using wish \
+exec cg source "$0" ${1+"$@"}
+
+proc findfield {fields pattern} {
+	lindex $fields [lsearch -glob $fields $pattern]
+}
+
+proc cg_homwes_compare args {
+	set mutireg {}
+#	set pos 0
+#	foreach {key value} $args {
+#		switch -- $key {
+#			-mutireg {
+#				set mutireg $value
+#			}
+#			default break
+#		}
+#		incr pos 2
+#	}
+#	set args [lrange $args $pos end]
+	set len [llength $args]
+	if {$len < 2} {
+		errorformat homwes_compare
+		exit 1
+	}
+	set resultfile [lindex $args 0]
+	set args [lrange $args 1 end]
+	if {[file exists $resultfile]} {file rename -force $resultfile $resultfile.old}
+	if {[file exists $resultfile.summary.tsv]} {file rename -force $resultfile.summary.tsv $resultfile.summary.tsv.old}
+	cg multireg $resultfile {*}$args
+	set o [open $resultfile.summary.tsv w]
+	puts $o [join {name snps numregions covered common sensitivity specificity fp fn fpvscovered fnvsrefcovered} \t]
+	set reffile [lindex $args 0]
+	set refname [file tail [file root $reffile]]
+	set refcovered [lindex [cg covered $reffile] end]
+	foreach file $args {
+		set name [file tail [file root $file]]
+		putslog "Analysing $name"
+		set numregions [lindex [cg select -g all $file] end]
+		set covered [lindex [cg covered $file] end]
+		set filtered [glob -nocomplain [file root $file].work/$name*-filtered.tsv]
+		if {[llength $filtered] == 1} {
+			set snps [lindex [cg select -g all [lindex $filtered 0]] end]
+		} else {
+			set snps NA
+		}
+		set common [lindex [exec cg select -q "\$$refname == 1 and \$$name == 1" $resultfile | cg covered] end]
+		set sensitivity [expr {100.0*$common/$refcovered}]
+		set specificity [expr {100.0*$common/$covered}]
+		set fp [lindex [exec cg select -q "\$$refname == 0 and \$$name == 1" $resultfile | cg covered] end]
+		set fn [lindex [exec cg select -q "\$$refname == 1 and \$$name == 0" $resultfile | cg covered] end]
+		set fppercent [expr {100.0*$fp/$covered}]
+		set fnpercent [expr {100.0*$fn/$refcovered}]
+		puts $o [join [list $name $snps $numregions $covered $common $sensitivity $specificity $fp $fn $fppercent $fnpercent] \t]
+	}
+	if {$o ne "stdout"} {close $o}
+}
