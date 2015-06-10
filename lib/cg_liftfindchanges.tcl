ee5f2ae67cdd61bc01e4ea93ba042026550b3a75
Peter.DeRijk <Peter.DeRijk@gmail.com>
2015-06-10 16:50:53 +0000

    added cg_liftfindchanges

---
+++ lib/cg_liftfindchanges.tcl
@@ -0,0 +1,49 @@
+proc cg_liftfindchanges {args} {
+	if {([llength $args] != 3)} {
+		exiterror "format is: cg liftfindchanges srcgenome destgenome liftoverfile"
+		exit 1
+	}
+	foreach {srcgenome destgenome liftoverfile} $args break
+	if {[file ext $liftoverfile] eq ".chain"} {
+		set useliftoverfile [file root $liftoverfile].tsv
+		if {![file exists $useliftoverfile]} {
+			cg chain2tsv $liftoverfile $useliftoverfile
+		}
+		set liftoverfile $useliftoverfile
+	}
+	set gs [genome_open $srcgenome]
+	set gd [genome_open $destgenome]
+	catch {close $fl}
+	set f [gzopen $liftoverfile]
+	set header [tsv_open $f]
+	if {$header ne {chromosome begin end strand destchromosome destbegin destend deststrand}} {
+		exiterror "header of file $liftoverfile should be: chromosome begin end strand destchromosome destbegin destend deststrand"
+	}
+	puts [join {chromosome begin end ref destchromosome destbegin destend destref} \t]
+	while 1 {
+		if {[gets $f line] == -1} break
+		set line [split $line \t]
+		foreach {chromosome begin end strand destchromosome destbegin destend deststrand} $line break
+		if {[catch {
+			set sseq [string toupper [genome_get $gs $chromosome $begin $end]]
+		} msg]} {
+			continue
+		}
+		if {[catch {
+			set dseq [string toupper [genome_get $gd $destchromosome $destbegin $destend]]
+		} msg]} {
+			continue
+		}
+		if {$sseq ne $dseq} {
+ 			if {[expr {($end-$begin)-($destend-$destbegin)}]} {
+				error "difference in length between matching blocks not supported"
+			}
+			foreach s [split $sseq ""] d [split $dseq ""] {
+				if {$s ne $d} {
+					puts [join [list $chromosome $begin [expr {$begin+1}] $s $destchromosome $destbegin [expr {$destbegin+1}] $d]\t]
+				}
+				incr begin ; incr destbegin
+			}
+		}
+	}	
+}
