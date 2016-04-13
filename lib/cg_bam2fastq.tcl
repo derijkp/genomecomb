91d0e950efea6e8d6b00dd945d75deaf519f44d5
Peter.DeRijk <Peter.DeRijk@gmail.com>
2016-04-13 08:18:15 +0000

    cg_bam2fastq fix: destprefix error

--- lib/cg_bam2fastq.tcl
+++ lib/cg_bam2fastq.tcl
@@ -1,5 +1,4 @@
 proc cg_bam2fastq {bamfile fastqfile1 {fastqfile2 {}}} {
-	set dir [file dir $destprefix]
 	if {$fastqfile2 ne ""} {
 		exec java -jar [picard]/SamToFastq.jar I=$bamfile F=$fastqfile1 F2=$fastqfile2 >@ stdout 2>@ stderr
 	} else {
