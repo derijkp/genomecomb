1a3e7eec2c33d4f50f505d4c482f456b63cf4ceb
Peter.DeRijk <Peter.DeRijk@gmail.com>
2016-06-01 16:50:29 +0000

    moved searchpath, picard, etc. to separate file extern.tcl

--- lib/cg_process_illumina.tcl
+++ lib/cg_process_illumina.tcl
@@ -82,56 +82,6 @@ proc cg_process_conv_illnextseq {illsrc destdir} {
 	cd $keeppwd
 }
 
-proc searchpath {envvar args} {
-	set name [lindex $args 0]
-	if {[info exists ::env($envvar)]} {
-		if {![file exists $::env($envvar)]} {
-			error "$name not found at $::env($envvar) (from env var $envvar)"
-		}
-		return $::env($envvar)
-	} else {
-		set dirlist [split [get ::env(PATH) ""] :]
-		list_addnew dirlist $::externdir
-		foreach pattern $args {
-			foreach dir $dirlist {
-				if {![catch {glob $dir/$pattern} dirs]} {
-					return [lindex [lsort -dict $dirs] 0]
-				}
-			}
-		}
-		error "$name not found in PATH"
-	}
-}
-
-proc picard {cmd args} {
-	global picard
-	if {![info exists picard]} {
-		set picard [searchpath PICARD picard picard*]
-	}
-	exec java -jar $picard/$cmd.jar {*}$args
-}
-
-proc gatk {} {
-	global gatk
-	if {![info exists gatk]} {
-		set gatk [searchpath GATK gatk GenomeAnalysisTK*]/GenomeAnalysisTK.jar
-	}
-	return $gatk
-}
-
-proc bcl2fastq {} {
-	global bcl2fastq
-	if {![info exists bcl2fastq]} {
-		set path [searchpath bcl2fastq bcl2fastq*]
-		if {[file isdir $path]} {
-			set bcl2fastq $path/bin/bcl2fastq
-		} else {
-			set bcl2fastq $path
-		}
-	}
-	return $bcl2fastq
-}
-
 proc fastq_clipadapters {files targets args} {
 	set adapterfile {}
 	set removeskew 1
