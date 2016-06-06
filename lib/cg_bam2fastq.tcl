262ef92d768264a35603b216bfb3c21a3665910d
Peter.DeRijk <Peter.DeRijk@gmail.com>
2016-06-06 09:29:44 +0000

    cg bam2fastq: added -m option (using the new sam method does not work properly on many bams though)

--- lib/cg_bam2fastq.tcl
+++ lib/cg_bam2fastq.tcl
@@ -1,4 +1,31 @@
-proc cg_bam2fastq {bamfile fastqfile1 {fastqfile2 {}}} {
+proc cg_bam2fastq {args} {
+	set pos 0
+	set method picard
+	while 1 {
+		set key [lindex $args $pos]
+		switch -- $key {
+			-m {
+				incr pos
+				set method [lindex $args $pos]
+			}
+			-- {
+				incr pos
+				break
+			}
+			default {
+				if {[string index $key 0] eq "-"} {error "unknown option \"$key\""}
+				break
+			}
+		}
+		incr pos
+	}
+	set args [lrange $args $pos end]
+	if {[llength $args] < 2 || [llength $args] > 3} {
+		errorformat bam2fastq
+		exit 1
+	}
+	set fastqfile2{}
+	foreach {bamfile fastqfile1 fastqfile2} $args break
 	set destdir [file dir $fastqfile1]
 	set tempbam $destdir/[file tail $bamfile].temp_namesorted.bam
 	# Aligning the generated fastq files may give problems/biases if the bam is sorted on position
@@ -11,17 +38,27 @@ proc cg_bam2fastq {bamfile fastqfile1 {fastqfile2 {}}} {
 #			error $msg
 #		}
 #	}
-	if {[catch {
-		if {$fastqfile2 ne ""} {
-			exec java -jar [picard]/SamToFastq.jar I=$tempbam F=$fastqfile1 F2=$fastqfile2 VALIDATION_STRINGENCY=SILENT >@ stdout
-		} else {
-			exec java -jar [picard]/SamToFastq.jar I=$tempbam F=$fastqfile1 VALIDATION_STRINGENCY=SILENT >@ stdout
+	if {$method eq "picard"} {
+		if {[catch {
+			if {$fastqfile2 ne ""} {
+				exec java -jar [picard]/SamToFastq.jar I=$tempbam F=$fastqfile1 F2=$fastqfile2 VALIDATION_STRINGENCY=SILENT >@ stdout
+			} else {
+				exec java -jar [picard]/SamToFastq.jar I=$tempbam F=$fastqfile1 VALIDATION_STRINGENCY=SILENT >@ stdout
+			}
+		} msg]} {
+			if {![regexp "done. Elapsed time:" $msg]} {
+				error $msg
+			}
 		}
-	} msg]} {
-		if {![regexp "done. Elapsed time:" $msg]} {
-			error $msg
-		}
+		puts stderr $msg
+	} elseif {$method eq "sam"} {
+		# just a try; this does not always keep mate pairs synced, so not very reliable
+		exec samtools view -uf64 x.bam | samtools bam2fq - > $fastqfile1.temp
+		exec samtools view -uf128 x.bam | samtools bam2fq - > $fastqfile2.temp
+		file rename $fastqfile1.temp $fastqfile1
+		file rename $fastqfile2.temp $fastqfile2
+	} else {
+		error "unknown method \"$method\", must be picard or sam"
 	}
-	puts stderr $msg
 	file delete $tempbam
 }
