6424f3b677f8f2f19c83ee1b3b1c41e81763a8cf
Peter.DeRijk <Peter.DeRijk@gmail.com>
2016-09-13 11:04:25 +0000

    cg lz4: added -blocksize option, use default blocksize 5

--- lib/cg_lz4.tcl
+++ lib/cg_lz4.tcl
@@ -2,6 +2,7 @@ proc cg_lz4 args {
 	set pos 0
 	set keep 0
 	set compressionlevel 9
+	set blocksize 5
 	foreach {key value} $args {
 		switch -- $key {
 			-k {
@@ -9,6 +10,9 @@ proc cg_lz4 args {
 			}
 			-c {
 				set compressionlevel $value
+			}
+			-b {
+				set blocksize $value
 			}
 			default {
 				break
@@ -24,7 +28,7 @@ proc cg_lz4 args {
 				putslog "lz4 $file"
 				set result [file root $file].lz4
 				exec gunzip -d -c $file > $result.temp2
-				exec lz4c -$compressionlevel -c $result.temp2 > $result.temp
+				exec lz4c -$compressionlevel -B$blocksize -c $result.temp2 > $result.temp
 				file delete $result.temp2
 				file rename -force $result.temp $result
 				if {!$keep} {file delete $file}
@@ -33,7 +37,7 @@ proc cg_lz4 args {
 				putslog "lz4 $file"
 				set result [file root $file].lz4
 				exec razip -d -c $file > $result.temp2
-				exec lz4c -$compressionlevel -c $result.temp2 > $result.temp
+				exec lz4c -$compressionlevel -B$blocksize -c $result.temp2 > $result.temp
 				file delete $result.temp2
 				file rename -force $result.temp $result
 				if {!$keep} {file delete $file}
@@ -46,14 +50,14 @@ proc cg_lz4 args {
 				putslog "lz4c $file"
 				set result [file root $file].lz4
 				exec bzcat $file > $result.temp2
-				exec lz4c -$compressionlevel -c $result.temp2 > $result.temp
+				exec lz4c -$compressionlevel -B$blocksize -c $result.temp2 > $result.temp
 				file delete $result.temp2
 				file rename -force $result.temp $result
 				if {!$keep} {file delete $file}
 			}
 			default {
 				putslog "lz4c $file"
-				exec lz4c -$compressionlevel -c $file > $file.lz4.temp
+				exec lz4c -$compressionlevel -B$blocksize -c $file > $file.lz4.temp
 				file rename -force $file.lz4.temp $file.lz4
 				if {!$keep} {file delete $file}
 			}
