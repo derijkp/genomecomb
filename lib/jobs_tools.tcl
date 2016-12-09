ec2be82b2e4314fe6f00cc143364925d449e9289
Peter.DeRijk <Peter.DeRijk@gmail.com>
2016-12-09 09:36:30 +0000

    cleanup_job fix: make optional

--- lib/jobs_tools.tcl
+++ lib/jobs_tools.tcl
@@ -2,7 +2,7 @@ proc cleanup_job {name rmtargets args} {
 	upvar job_logdir job_logdir
 	set num 1
 	foreach deps $args {
-		job cleanup-$name-deps$num -deps [list {*}$deps {*}[job_optdeps $rmtargets]] -vars {rmtargets} \
+		job cleanup-$name-deps$num -optional 1 -deps [list {*}$deps {*}[job_optdeps $rmtargets]] -vars {rmtargets} \
 			-rmtargets $rmtargets -code {
 			foreach file $rmtargets {
 				catch {file delete $file}
