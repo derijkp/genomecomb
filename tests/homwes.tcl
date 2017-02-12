addde3a208a3bdb64515643497bb62b1524d9a7f
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-02-12 09:39:30 +0000

    tests fix: homwes test did not use refseqtest/hg19

--- tests/homwes.tcl
+++ tests/homwes.tcl
@@ -10,7 +10,7 @@ test homwes {basic} {
 	file delete -force $::bigtestdir/tmp/homwes
 	file mkdir $::bigtestdir/tmp/homwes
 	set varfile $::bigtestdir/ori/exomes_yri/compar/annot_compar-yri_exome_test.tsv.lz4
-	set dbdir $::bigtestdir/refseq/hg19
+	set dbdir $::bigtestdir/refseqtest/hg19
 	cg homwes --stack 1 -dbdir $dbdir $varfile NA19240 $::bigtestdir/tmp/homwes/homwes-out.tsv
 	checkdiff -qr $::bigtestdir/tmp/homwes $::bigtestdir/expected/yri_exome.homwes-out
 } [subst {Files $::bigtestdir/tmp/homwes/homwes-out.work/homwes-out-NA19240.log and $::bigtestdir/expected/yri_exome.homwes-out/homwes-out.work/homwes-out-NA19240.log differ
