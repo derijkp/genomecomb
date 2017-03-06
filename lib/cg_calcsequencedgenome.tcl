5265f067e89c3983e0e2dc44dc2a917c9364b9ec
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-03-06 15:19:37 +0000

    cg_calcsequencedgenome fix: extra chromosome names were wrong, add size

--- lib/cg_calcsequencedgenome.tcl
+++ lib/cg_calcsequencedgenome.tcl
@@ -11,11 +11,7 @@ proc cg_calcsequencedgenome {args} {
 	while {![eof $f]} {
 		set name [gets $f]
 		if {$name eq ""} continue
-		if {![regexp {chromosome ([0-9A-Z]+)} $name temp chr]} {
-			if {![regexp {chr([0-9A-Z]+)} $name temp chr]} {
-				set chr $name
-			}
-		}
+		set chr [chr_clip [lindex [string range $name 1 end] 0]]
 		putslog $name\n$chr
 		set seq [gets $f]
 		set indices [regexp -all -inline -indices {[^N]{1,}} $seq]
@@ -24,12 +20,12 @@ proc cg_calcsequencedgenome {args} {
 		list_foreach {begin end} $indices {
 			incr end
 			if {$begin > [expr {$pend+1}]} {
-				puts $o chr$chr\t$pbegin\t$pend
+				puts $o chr$chr\t$pbegin\t$pend\t[expr {$end-$begin}]
 				set pbegin $begin
 			}
 			set pend $end
 		}
-		puts $o chr$chr\t$pbegin\t$pend
+		puts $o chr$chr\t$pbegin\t$pend\t[expr {$end-$begin}]
 	}
 	close $o
 	close $f
