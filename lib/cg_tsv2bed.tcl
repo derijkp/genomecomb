06cd4a99764e23a84cc32ed4a7345dc45fe96f39
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-03-15 17:14:35 +0000

    gatkworkaround_tsv2bed also separate from gatkworkaround_tsv2bed_job

--- lib/cg_tsv2bed.tcl
+++ lib/cg_tsv2bed.tcl
@@ -1,36 +1,42 @@
+# job, removes when end=begin, clips chromosome, 
+proc gatkworkaround_tsv2bed {reg refseq target} {
+	set f [open $refseq.index]
+	while {![eof $f]} {
+		set line [split [gets $f] \t]
+		set chr [lindex $line 0]
+		set chr [chr_clip $chr]
+		set maxa($chr) [lindex $line 1 1]
+	}
+	close $f
+	set f [gzopen $reg]
+	set header [tsv_open $f]
+	set poss [tsv_basicfields $header 3]
+	set temptarget [filetemp $target]
+	set o [open $temptarget w]
+	while {![eof $f]} {
+		set line [split [gets $f] \t]
+		set line [list_sub $line $poss]
+		foreach {chr begin end} $line break
+		set cchr [chr_clip $chr]
+		if {[info exists maxa($cchr)]} {
+			if {$end > $maxa($cchr)} {set end $maxa($cchr)}
+		}
+		if {$end == $begin} continue
+		puts $o $chr\t$begin\t$end
+	}
+	close $o
+	gzclose $f
+	file rename -force $temptarget $target
+}
+
 # job, removes when end=begin, clips chromosome
 proc gatkworkaround_tsv2bed_job {file refseq} {
 	upvar job_logdir job_logdir
-	job tsv2bed-[file tail $file] -optional 1 -deps {$file $refseq.index} -targets [file root $file].bed -code {
-		set f [open $dep2]
-		while {![eof $f]} {
-			set line [split [gets $f] \t]
-			set chr [lindex $line 0]
-			set chr [chr_clip $chr]
-			set maxa($chr) [lindex $line 1 1]
-		}
-		close $f
-		set f [gzopen $dep]
-		set header [tsv_open $f]
-		set poss [tsv_basicfields $header 3]
-		set temptarget [filetemp $target]
-		set o [open $temptarget w]
-		while {![eof $f]} {
-			set line [split [gets $f] \t]
-			set line [list_sub $line $poss]
-			foreach {chr begin end} $line break
-			set cchr [chr_clip $chr]
-			if {[info exists maxa($cchr)]} {
-				if {$end > $maxa($cchr)} {set end $maxa($cchr)}
-			}
-			if {$end == $begin} continue
-			puts $o $chr\t$begin\t$end
-		}
-		close $o
-		gzclose $f
-		file rename -force $temptarget $target
+	set target [file root $file].bed
+	job tsv2bed-[file tail $file] -optional 1 -deps {$file $refseq} -targets $target -code {
+		gatkworkaround_tsv2bed $dep $dep2 $target
 	}
-	return [file root $file].bed
+	return $target
 }
 
 proc tsv2bed {tsvfile bedfile {fields {}}} {
