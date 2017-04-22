69464f898459d98077988959c9ecd69f648f00c0
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-04-22 08:56:09 +0000

    version to 0.98.3

--- tests/alldistr.tcl
+++ tests/alldistr.tcl
@@ -3,7 +3,7 @@
 exec tclsh "$0" "$@"
 
 # $Format: "set version $ProjectMajorVersion$.$ProjectMinorVersion$.$ProjectPatchLevel$"$
-set version 0.98.2
+set version 0.98.3
 
 set keeppath $::env(PATH)
 set testlist [list /home/peter/build/genomecomb-$version-Linux-i686 /home/peter/build/genomecomb-$version-Linux-x86_64]
