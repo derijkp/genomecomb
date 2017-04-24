ec874373b603947d5dbd7b1656dfa18da24e83d6
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-04-24 08:05:12 +0000

    job_update fix: avoid error when cgjob(basedir) is not defined

--- lib/cg_job_update.tcl
+++ lib/cg_job_update.tcl
@@ -80,7 +80,7 @@ proc job_update {logfile {cleanup success}} {
 			puts $o [join [list $job $jobid $endstatus $submittime $endstarttime $endendtime [timediff2duration $totalduration] $targets [job_cleanmsg $msg] $run] \t]
 			break
 		}
-		if {$cgjob(basedir) ne "" && [file pathtype $jobo] ne "absolute"} {set job $cgjob(basedir)/$jobo} else {set job $jobo}
+		if {[get cgjob(basedir) ""] ne "" && [file pathtype $jobo] ne "absolute"} {set job $cgjob(basedir)/$jobo} else {set job $jobo}
 		if {$status in {submitted running}} {set starttime {} ; set endtime {} ; set duration {}}
 		if {($starttime eq "" || $endtime eq "" | $duration eq "") && [job_file_exists $job.log]} {
 			set jobloginfo [job_parse_log $job $totalduration]
