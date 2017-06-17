e91e812d6af64edc8e84e6376c5845ba1155dc85
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-06-17 12:53:27 +0000

    annotatebcolvar: commented out debug puts

--- lib/annotate_bcol.tcl
+++ lib/annotate_bcol.tcl
@@ -58,7 +58,7 @@ proc annotatebcolvar {file dbfile name annotfile} {
 	if {[gziscompressed $file]} {
 		error "bcol_annot not supported for compressed files"
 	}
-	 puts "bcol_annot $file $poss $dbfile -1"
+	# puts "bcol_annot $file $poss $dbfile -1"
 	if {[catch {
 		exec bcol_annot $file {*}$poss $dbfile -1 >> $annotfile.temp 2>@ stderr
 	} error]} {
