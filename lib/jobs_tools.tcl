c4658e98c7f35d1db812e14e56a27e905c262eec
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-07-18 13:17:28 +0000

    cleanup_job fix: do not add rmtargets as deps

--- lib/jobs_tools.tcl
+++ lib/jobs_tools.tcl
@@ -7,7 +7,7 @@ proc cleanup_job {name rmtargets args} {
 	if {!$todo} return
 	set num 1
 	foreach deps $args {
-		job cleanup-$name-deps$num -optional 1 -deps [list {*}$deps {*}[job_optdeps $rmtargets]] -vars {rmtargets} \
+		job cleanup-$name-deps$num -optional 1 -deps [list {*}$deps] -vars {rmtargets} \
 			-rmtargets $rmtargets -code {
 			foreach file $rmtargets {
 				catch {file delete $file}
