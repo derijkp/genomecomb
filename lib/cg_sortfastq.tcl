7bbd01f49fa8ccb5889046e1e7c49432872e3072
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-08-08 02:41:24 +0000

    added cg_sortfastq

---
+++ lib/cg_sortfastq.tcl
@@ -0,0 +1,35 @@
+proc cg_sortfastq {args} {
+	cg_options sortfastq args {
+	} {infile outfile} 0 2
+	if {![info exists infile]} {
+		exec paste - - - - | gnusort8 -T [scratchdir] -t \t -N -s -k 1,1 | tr {\t} {\n} <@ stdin >@ stdout
+	} elseif {![info exists outfile]} {
+		exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 -T [scratchdir] -t \t -N -s -k 1,1 | tr {\t} {\n} >@ stdout
+	} else {
+		exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 -T [scratchdir] -t \t -N -s -k 1,1 | tr {\t} {\n} {*}[compresspipe $outfile] > $outfile.temp
+		file rename -force $outfile.temp $outfile
+	}
+}
+
+proc cg_fastq2tsv {args} {
+	cg_options fastq2tsv args {
+	} {infile outfile} 0 2
+	set header [join {id sequence temp quality} \t]
+	if {[info exists outfile]} {
+		set o [open [list {*}[compresspipe $outfile] > $outfile.temp] w]
+	} else {
+		set o stdout
+	}
+	if {![info exists infile]} {
+		puts $header
+		exec paste - - - - <@ stdin >@ stdout
+	} elseif {![info exists outfile]} {
+		puts $header
+		exec {*}[gzcat $infile] $infile | paste - - - - >@ stdout
+	} else {
+		puts $o $header
+		exec {*}[gzcat $infile] $infile | paste - - - - >@ $o
+		close $o
+		file rename -force $outfile.temp $outfile
+	}
+}
