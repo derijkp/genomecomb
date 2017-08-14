e70c81f6a624bcaacae56b00ebf3cc10230c2922
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-08-14 21:03:16 +0000

    samtools_sort fix: make tempfile first

--- lib/samtools.tcl
+++ lib/samtools.tcl
@@ -4,13 +4,18 @@ proc samtools_sort {args} {
 	set resultfile [lindex $args end]
 	set args [lrange $args 0 end-2]
 	if {[catch {version samtools 1}]} {
-		if {[catch {exec samtools sort {*}$args $bamfile $resultfile 2>@ stdout} msg]} {
+		if {[catch {exec samtools sort {*}$args $bamfile $resultfile.temp 2>@ stdout} msg]} {
 			error $msg
 		}
-		file rename -force $resultfile.bam $resultfile
+		if {[file exists $resultfile.temp.bam]} {
+			file rename -force $resultfile.temp.bam $resultfile
+		} else {
+			file rename -force $resultfile.temp $resultfile
+		}
 	} else {
-		if {[catch {exec samtools sort {*}$args $bamfile > $resultfile 2>@ stdout} msg]} {
+		if {[catch {exec samtools sort {*}$args $bamfile > $resultfile.temp 2>@ stdout} msg]} {
 			error $msg
 		}
+		file rename -force $resultfile.temp $resultfile
 	}
 }
