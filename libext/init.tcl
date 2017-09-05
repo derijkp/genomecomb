6157a52d377f8748a05f75561d7703eee107dbbe
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-09-05 14:18:29 +0000

    version number to 0.98.5

--- libext/init.tcl
+++ libext/init.tcl
@@ -12,7 +12,7 @@ namespace eval genomecomb {}
 # $Format: "set ::genomecomb::version $ProjectMajorVersion$.$ProjectMinorVersion$"$
 set ::genomecomb::version 0.98
 # $Format: "set ::genomecomb::patchlevel $ProjectPatchLevel$"$
-set ::genomecomb::patchlevel 4
+set ::genomecomb::patchlevel 5
 
 package provide genomecomb $::genomecomb::version
 
