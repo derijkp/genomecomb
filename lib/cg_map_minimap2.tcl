dff02e3b6a52527c0ea9c4aa07021897113307be
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-10-18 14:03:29 +0000

    cg minimap2: added option -keepsams, logfile

--- lib/cg_map_minimap2.tcl
+++ lib/cg_map_minimap2.tcl
@@ -5,18 +5,21 @@ proc minimap2refseq_job {refseq preset} {
 	set tail [file tail $refseq]
 	if {[jobtargetexists [list $minimap2refseq] $refseq]} return
 	job minimap2_2refseq-[file tail $refseq] -deps {$refseq} -targets {$minimap2refseq} -vars {preset} -code {
-		if {[catch {exec -ignorestderr minimap2 -x $preset -d $target $dep} e]} {
+		if {[catch {exec -ignorestderr minimap2 -x $preset -d $target.temp $dep} e]} {
 			error $e
 		}
+		file rename $target.temp $target
 	}
 	return $minimap2refseq
 }
 
 proc map_minimap2_job {args} {
 	upvar job_logdir job_logdir
+	set keepargs $args
 	set preset map-ont
 	set readgroupdata {}
 	set skips {}
+	set keepsams 0
 	set threads 2
 	cg_options map_minimap2 args {
 		-x - -preset - -p {
@@ -24,6 +27,9 @@ proc map_minimap2_job {args} {
 		}
 		-readgroupdata {
 			set readgroupdata $value
+		}
+		-keepsams {
+			set keepsams [true $value]
 		}
 		-skips {
 			set skips $value
@@ -33,9 +39,17 @@ proc map_minimap2_job {args} {
 		}
 	} {result refseq sample fastqfile1} 4
 	set files [list $fastqfile1 {*}$args]
+	set result [file_absolute $result]
+	set refseq [file_absolute $refseq]
+	set resultdir [file dir $result]
+	file mkdir $result.index
 	if {![info exists job_logdir]} {
-		job_logdir [file dir $result]/log_jobs
+		job_logdir $result.index/log_jobs
 	}
+	job_logfile $resultdir/map_minmap2_[file tail $result] $resultdir \
+		[list cg map_minmap2_ {*}$keepargs] \
+		{*}[versions minimap2]
+	#
 	array set a [list PL illumina LB solexa-123 PU $sample SM $sample]
 	if {$readgroupdata ne ""} {
 		array set a $readgroupdata
@@ -47,10 +61,10 @@ proc map_minimap2_job {args} {
 	set num 1
 	foreach file $files {
 		set name [file root [file tail $file]]
-		set target $resultbase-$name.sam
+		set target $result.index/$name.sam
 		lappend samfiles $target
 		job minimap2-$sample-$name -mem 5G -cores $threads \
-		-deps [list $minimap2refseq $file] -targets {$target} \
+		-deps {$minimap2refseq $file} -targets {$target} \
 		-vars {threads preset readgroupdata sample} \
 		-skip [list $resultbase.bam] {*}$skips -code {
 			puts "making $target"
@@ -64,7 +78,9 @@ proc map_minimap2_job {args} {
 			file rename -force $target.temp $target
 		}
 	}
-	job minimap2_2bam-$sample -deps $samfiles -rmtargets $samfiles -targets {$result $result.bai} {*}$skips -vars {resultbase} -code {
+	if {$keepsams} {set rmsamfiles {}} else {set rmsamfiles $samfiles}
+	job minimap2_2bam-$sample -deps $samfiles -rmtargets $rmsamfiles \
+	-targets {$result $result.bai} {*}$skips -vars {resultbase} -code {
 		puts "making $target"
 		if {[catch {
 			exec samcat {*}$deps | bamsort SO=coordinate tmpfile=[scratchfile] index=1 indexfilename=$target.bai inputformat=sam > $target.temp 2>@ stderr
@@ -72,7 +88,7 @@ proc map_minimap2_job {args} {
 			error $msg
 		}
 		file rename -force $target.temp $target
-		file delete {*}$deps
+		if {[llength $deps]} {file delete {*}$deps}
 	}
 }
 
