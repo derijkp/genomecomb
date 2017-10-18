de1b58da52cf8d3e38db79e02b64d0a005a10bf5
Peter.DeRijk <Peter.DeRijk@gmail.com>
2017-10-18 14:02:19 +0000

    albacore wip: -subdirs

--- lib/cg_basecall_albacore.tcl
+++ lib/cg_basecall_albacore.tcl
@@ -1,7 +1,8 @@
 proc basecaller_albacore_job {args} {
 	upvar job_logdir job_logdir
 	set keepargs $args
-	set reads 50000
+	set numreads 4000
+	set subdirs 1
 	set config {}
 	set flowcell {}
 	set kit {}
@@ -9,8 +10,11 @@ proc basecaller_albacore_job {args} {
 	set threads 1
 	set barcoding 0
 	cg_options var_sam args {
-		-r - -reads {
-			set reads $value
+		-n - -numreads {
+			set numreads $value
+		}
+		-s - -subdirs {
+			set subdirs $value
 		}
 		-c - -config {
 			set config $value
@@ -36,9 +40,12 @@ proc basecaller_albacore_job {args} {
 		default {
 			lappend opts $key $value
 		}
-	} {sourcedir resultdir}
-	set sourcedir [file_absolute $sourcedir]
+	} {resultdir sourcedir} 2
 	set resultdir [file_absolute $resultdir]
+	set sourcedirs [list [file_absolute $sourcedir]]
+	foreach dir $args {
+		lappend sourcedirs [file_absolute $dir]
+	}
 	if {$config eq ""} {
 		if {$kit eq "" || $flowcell eq ""} {
 			error "Either -config or -flowcell and -kit must be specified"
@@ -47,41 +54,89 @@ proc basecaller_albacore_job {args} {
 	} else {
 		lappend opts --config $config
 	}
-	job_logfile $resultdir/basecall_albacore_[file tail $resultdir] $resultdir \
+	if {![info exists job_logdir]} {
+		job_logdir $resultdir/log_jobs
+	}
+	set name [file tail $resultdir]
+	if {$name eq "fastq"} {
+		set name [file tail [file dir $resultdir]]
+	}
+	job_logfile $resultdir/basecall_albacore_$name $resultdir \
 		[list cg basecaller_albacore {*}$keepargs] \
 		{*}[versions albacore]
-	# find fast5 files
-	set files [lsort -dict [dirglob $sourcedir *.fast5]]
-	set len [llength $files]
-	file mkdir $resultdir
-	file_write $resultdir/info.txt "source\t$sourcedir\nsize\t$len\n"
-	for {set from 0} {$from < $len} {incr from $reads} {
-		set to [expr {$from+$reads-1}]
-		if {$to > $len} {set to $len}
-		set todo [lrange $files $from $to]
-		set target1 $resultdir/pass_[file tail $resultdir]-$from-$to.fastq.gz
-		set target2 $resultdir/fail_[file tail $resultdir]-$from-$to.fastq.gz
-		job albacore-[file tail $sourcedir]-$from-$to -cores $threads \
-		-deps $sourcedir \
-		-targets {$target1 $target2} \
-		-vars {sourcedir from to todo opts threads} \
-		-code {
-			set tempdir [scratchdir]
-			file mkdir $tempdir/fast5
-			foreach file $todo {
-				mklink $sourcedir/$file $tempdir/fast5/$file 1
+
+	if {$subdirs} {
+		foreach sourcedir $sourcedirs {
+			set sourcename [file tail $sourcedir]
+			# find subdirs
+			set dirs [lsort -dict [dirglob $sourcedir fast5/*]]
+			file mkdir $resultdir
+			file_write $resultdir/info.txt "source\t$sourcedir\nsubdirs\t[llength $dirs]\n"
+			foreach dir $dirs {
+				set files [dirglob $sourcedir/$dir *.fast5]
+				set len [llength $files]
+				if {$subdirs == 1 && $len < $numreads} {
+					puts "skipping $dir: not full yet"
+					continue
+				}
+				set name ${sourcename}_[file tail $dir]
+				set target1 $resultdir/pass_$name.fastq.gz
+				set target2 $resultdir/fail_$name.fastq.gz
+				job albacore-[file tail $sourcedir]-$name -cores $threads \
+				-deps {$sourcedir/$dir} \
+				-targets {$target1 $target2} \
+				-vars {sourcedir from to todo opts threads} \
+				-code {
+					set tempdir [scratchdir]
+					exec read_fast5_basecaller.py {*}$opts \
+						--files_per_batch_folder 0 \
+						--reads_per_fastq_batch 0 \
+						-t $threads -o fastq \
+						-i $dep \
+						-s $tempdir/tempfastq >@ stdout 2>@ stderr
+					exec cat {*}[glob $tempdir/tempfastq/workspace/pass/*.fastq] | gzip > $target1
+					exec cat {*}[glob $tempdir/tempfastq/workspace/fail/*.fastq] | gzip > $target2
+					file delete -force $tempdir/fast5 $tempdir/tempfastq
+				}
 			}
-			exec read_fast5_basecaller.py {*}$opts \
-				--files_per_batch_folder 0 \
-				--reads_per_fastq_batch 0 \
-				-t $threads -o fastq \
-				-i $tempdir/fast5 \
-				-s $tempdir/tempfastq >@ stdout 2>@ stderr
-			exec cat {*}[glob $tempdir/tempfastq/workspace/pass/*.fastq] | gzip > $target1
-			exec cat {*}[glob $tempdir/tempfastq/workspace/fail/*.fastq] | gzip > $target2
-			file delete -force $tempdir/fast5 $tempdir/tempfastq
+		}
+	} else {
+		# find fast5 files
+		set files [lsort -dict [dirglob $sourcedir *.fast5]]
+		set len [llength $files]
+		if {$len == 0} {error "no fast5 files found (use -subdirs?)"}
+		file mkdir $resultdir
+		file_write $resultdir/info.txt "source\t$sourcedir\nsize\t$len\n"
+		for {set from 0} {$from < $len} {incr from $numreads} {
+			set to [expr {$from+$numreads-1}]
+			if {$to > $len} {set to $len}
+			set todo [lrange $files $from $to]
+			set target1 $resultdir/pass_[file tail $resultdir]-$from-$to.fastq.gz
+			set target2 $resultdir/fail_[file tail $resultdir]-$from-$to.fastq.gz
+			job albacore-[file tail $sourcedir]-$from-$to -cores $threads \
+			-deps $sourcedir \
+			-targets {$target1 $target2} \
+			-vars {sourcedir from to todo opts threads} \
+			-code {
+				albacoreinit
+				set tempdir [scratchdir]
+				file mkdir $tempdir/fast5
+				foreach file $todo {
+					mklink $sourcedir/$file $tempdir/fast5/$file 1
+				}
+				exec read_fast5_basecaller.py {*}$opts \
+					--files_per_batch_folder 0 \
+					--reads_per_fastq_batch 0 \
+					-t $threads -o fastq \
+					-i $tempdir/fast5 \
+					-s $tempdir/tempfastq >@ stdout 2>@ stderr
+				exec cat {*}[glob $tempdir/tempfastq/workspace/pass/*.fastq] | gzip > $target1
+				exec cat {*}[glob $tempdir/tempfastq/workspace/fail/*.fastq] | gzip > $target2
+				file delete -force $tempdir/fast5 $tempdir/tempfastq
+			}
 		}
 	}
+
 }
 
 proc cg_basecaller_albacore {args} {
