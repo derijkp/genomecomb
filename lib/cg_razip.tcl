5593f1731addf61d66f39200b1a8076b1b7cdb8c
Peter.DeRijk <Peter.DeRijk@gmail.com>
2018-03-30 14:33:08 +0000

    cg razip: added -d option

--- lib/cg_razip.tcl
+++ lib/cg_razip.tcl
@@ -1,50 +1,56 @@
 proc cg_razip args {
 	set pos 0
 	set keep 0
+	set args [job_init {*}$args]
 	cg_options razip args {
 		-k - -keep {
 			set keep 1
 		}
 	}
 	foreach file $args {
-		set ext [file extension $file]
-		switch $ext {
-			.gz {
-				putslog "razip $file"
-				set result [file root $file].rz
-				exec gunzip -d -c $file > $result.temp2
-				exec razip -c $result.temp2 > $result.temp
-				file delete $result.temp2
-				file rename -force $result.temp $result
-				if {!$keep} {file delete $file}
-			}
-			.rz {
-				putslog "$file already razip"
-			}
-			.lz4 {
-				putslog "razip $file"
-				set result [file root $file].rz
-				exec lz4c -q -d $file > $result.temp2
-				exec razip -c $result.temp2 > $result.temp
-				file delete $result.temp2
-				file rename -force $result.temp $result
-				if {!$keep} {file delete $file}
-			}
-			.bz2 {
-				putslog "razip $file"
-				set result [file root $file].rz
-				exec bzcat $file > $result.temp2
-				exec razip -c $result.temp2 > $result.temp
-				file delete $result.temp2
-				file rename -force $result.temp $result
-				if {!$keep} {file delete $file}
-			}
-			default {
-				putslog "razip $file"
-				exec razip -c $file > $file.rz.temp
-				file rename -force $file.rz.temp $file.rz
-				if {!$keep} {file delete $file}
+		set target [gzroot $file].rz
+		job razip-$file -deps {$file} -targets {$target} -vars {keep} -code {
+			set file $dep
+			set ext [file extension $file]
+			switch $ext {
+				.gz {
+					putslog "razip $file"
+					set result [file root $file].rz
+					exec gunzip -d -c $file > $result.temp2
+					exec razip -c $result.temp2 > $result.temp
+					file delete $result.temp2
+					file rename -force $result.temp $result
+					if {!$keep} {file delete $file}
+				}
+				.rz {
+					putslog "$file already razip"
+				}
+				.lz4 {
+					putslog "razip $file"
+					set result [file root $file].rz
+					exec lz4c -q -d $file > $result.temp2
+					exec razip -c $result.temp2 > $result.temp
+					file delete $result.temp2
+					file rename -force $result.temp $result
+					if {!$keep} {file delete $file}
+				}
+				.bz2 {
+					putslog "razip $file"
+					set result [file root $file].rz
+					exec bzcat $file > $result.temp2
+					exec razip -c $result.temp2 > $result.temp
+					file delete $result.temp2
+					file rename -force $result.temp $result
+					if {!$keep} {file delete $file}
+				}
+				default {
+					putslog "razip $file"
+					exec razip -c $file > $file.rz.temp
+					file rename -force $file.rz.temp $file.rz
+					if {!$keep} {file delete $file}
+				}
 			}
 		}
 	}
+	job_wait
 }
