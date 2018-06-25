4a3d490b7552ee0774e4c2c52f1841f30488a1c0
Peter.DeRijk <Peter.DeRijk@gmail.com>
2018-06-25 10:10:45 +0000

    tests map_bwa: check multiple fastqs

--- tests/map.tcl
+++ tests/map.tcl
@@ -14,6 +14,21 @@ test map_bwa {map_bwa basic} {
 	exec diff tmp/ali.sam tmp/expected.sam
 } {}
 
+test map_bwa {map_bwa multiple} {
+	test_cleantmp
+	set temp [split [string trim [exec zcat data/seq_R1.fq.gz]] \n]
+	file_write tmp/seq1_R1.fq [join [lrange $temp 0 199] \n]\n
+	file_write tmp/seq2_R1.fq [join [lrange $temp 200 end] \n]\n
+	set temp [split [string trim [exec zcat data/seq_R2.fq.gz]] \n]
+	file_write tmp/seq1_R2.fq [join [lrange $temp 0 199] \n]\n
+	file_write tmp/seq2_R2.fq [join [lrange $temp 200 end] \n]\n
+	cg map_bwa -stack 1 -paired 1 tmp/ali.bam $::refseqdir/hg19/genome_hg19.ifas NA19240m {*}[lsort -dict [glob tmp/*.fq]]
+	# chr21:42730799-42762826
+	exec samtools view tmp/ali.bam > tmp/ali.sam
+	exec samtools view data/bwa.bam > tmp/expected.sam
+	exec diff tmp/ali.sam tmp/expected.sam
+} {}
+
 test map_bowtie2 {map_bowtie2 basic} {
 	test_cleantmp
 	file copy data/seq_R1.fq.gz data/seq_R2.fq.gz tmp
