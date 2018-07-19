22863d4387dc8189202f4a946b4c581a28ed67fa
Peter.DeRijk <Peter.DeRijk@gmail.com>
2018-07-19 14:41:50 +0000

    cg proces_reportscombine change: per analysis results

--- lib/cg_process_reportscombine.tcl
+++ lib/cg_process_reportscombine.tcl
@@ -49,60 +49,46 @@ proc proces_reportscombine_job {args} {
 			file rename -force $target.temp2 $target
 			file delete $target.temp
 			# make report_summarytable
-			unset -nocomplain posa
-			set pos -1
-			set template {}
-			foreach {source parameter} {
-				fastq-stats	fw_numreads
-				fastq-stats	rev_numreads
-				flagstat_reads {in total}
-				flagstat_reads duplicates
-				flagstat_reads mapped
-				flagstat_reads {properly paired}
-				histodepth	targetbases
-				histodepth	pct_target_bases_2X
-				histodepth	pct_target_bases_10X
-				histodepth	pct_target_bases_20X
-				histodepth	pct_target_bases_30X
-				genomecomb	covered_total
-				genomecomb	qvars
-				genomecomb	qvars_target
-				genomecomb	qvars_refcoding
-			} {
-				set posa($source,$parameter) [incr pos]
-				lappend template {}
-			}
 			unset -nocomplain data
+			unset -nocomplain a
 			set f [open $target]
 			set header [tsv_open $f]
 			while {[gets $f line] != -1} {
-				foreach {sample source parameter value} [split $line \t] break
-				set sample [lindex [split $sample -] end]
+				foreach {analysis source parameter value} [split $line \t] break
+				set a($analysis) 1
 				set parameter [string trim $parameter]
-				if {![info exists data($sample)]} {
-					set data($sample) $template
-				}
-				if {[info exists posa($source,$parameter)]} {lset data($sample) $posa($source,$parameter) $value}
+				set data($analysis,$source,$parameter) $value
 			}
 			close $f
-
 			set o [open $target2.temp w]
 			puts $o [join {
-				sample numreads pf_reads pct_pf_reads pf_unique_reads pct_pf_unique_reads pf_mapped pct_pf_aligned_reads
+				analysis sample numreads 
+				pf_reads pct_pf_reads pf_unique_reads pct_pf_unique_reads pf_mapped pct_pf_aligned_reads
 				targetbases	pct_target_bases_2X pct_target_bases_10X pct_target_bases_20X pct_target_bases_30X
 				covered_total qvars qvars_target qvars_refcoding
 			} \t]
-			foreach sample [ssort -natural [array names data]] {
-				foreach {
-					fw_numreads rev_numreads pf_reads pf_duplicates pf_mapped pf_properlypaired
-					targetbases	pct_target_bases_2X pct_target_bases_10X pct_target_bases_20X pct_target_bases_30X
-					covered_total qvars qvars_target qvars_refcoding
-				} $data($sample) break
+			set analyses [ssort -natural [array names a]]
+			foreach analysis $analyses {
+				# only report for "endpoints" analysis (data of partial will be included in endpoints)
+				if {[regexp {^cov[0-9]+-} $analysis]} continue
+				if {[llength [array names a *-$analysis]]} continue
+				set sample [lindex [split $analysis -] end]
+				set resultline [list $analysis $sample]
+				# numreads
+				set fw_numreads [get data($sample,fastq-stats,fw_numreads) {}]
+				set rev_numreads [get data($sample,fastq-stats,rev_numreads) {}]
 				if {[isint $fw_numreads] && [isint $rev_numreads]} {
 					set numreads [expr {$fw_numreads+$rev_numreads}]
 				} else {
 					set numreads {}
 				}
+				lappend resultline $numreads
+				# bam based stats
+				set bamname [join [lrange [split $analysis -] end-1 end] -]
+				set pf_reads [get "data($bamname,flagstat_reads,in total)" {}]
+				set pf_duplicates [get "data($bamname,flagstat_reads,duplicates)" {}]
+				set pf_mapped [get "data($bamname,flagstat_reads,mapped)" {}]
+				set pf_properlypaired [get "data($bamname,flagstat_reads,properly paired)" {}]
 				if {[isint $numreads] && [isint $pf_reads]} {
 					set pct_pf_reads [format %.2f [expr {$pf_reads*100.0/$numreads}]]
 					set pf_unique_reads [expr {($pf_reads - $pf_duplicates)}]
@@ -114,8 +100,17 @@ proc proces_reportscombine_job {args} {
 					set pct_pf_unique_reads {}
 					set pct_pf_aligned_reads {}
 				}
-				set resultline [list $sample $numreads $pf_reads $pct_pf_reads $pf_unique_reads $pct_pf_unique_reads $pf_mapped $pct_pf_aligned_reads]
-				lappend resultline {*}[lrange $data($sample) 6 end]
+				lappend resultline $pf_reads $pct_pf_reads $pf_unique_reads $pct_pf_unique_reads $pf_mapped $pct_pf_aligned_reads
+				# rest
+				lappend resultline [get data($bamname,histodepth,targetbases) {}]
+				lappend resultline [get data($bamname,histodepth,pct_target_bases_2X) {}]
+				lappend resultline [get data($bamname,histodepth,pct_target_bases_10X) {}]
+				lappend resultline [get data($bamname,histodepth,pct_target_bases_20X) {}]
+				lappend resultline [get data($bamname,histodepth,pct_target_bases_30X) {}]
+				lappend resultline [get data($analysis,genomecomb,covered_total) {}]
+				lappend resultline [get data($analysis,genomecomb,qvars) {}]
+				lappend resultline [get data($analysis,genomecomb,qvars_target) {}]
+				lappend resultline [get data($analysis,genomecomb,qvars_refcoding) {}]
 				puts $o [join $resultline \t]
 			}
 			close $o
