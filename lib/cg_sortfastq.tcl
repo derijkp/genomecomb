21805954445449b51f5e9f3020f3dc128e1494ab
Peter De Rijk <Peter.DeRijk@gmail.com>
2019-12-27 11:15:31 +0100

    cg_sortfastq uses threads

--- lib/cg_sortfastq.tcl
+++ lib/cg_sortfastq.tcl
@@ -1,12 +1,14 @@
 proc cg_sortfastq {args} {
+	set threads 1
 	cg_options sortfastq args {
+		-threads {set threads $value}
 	} {infile outfile} 0 2
 	if {![info exists infile]} {
-		exec paste - - - - | gnusort8 -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} <@ stdin >@ stdout
+		exec paste - - - - | gnusort8 --parallel $threads-T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} <@ stdin >@ stdout
 	} elseif {![info exists outfile]} {
-		exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} >@ stdout
+		exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 --parallel $threads -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} >@ stdout
 	} else {
-		exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} {*}[compresspipe $outfile] > $outfile.temp
+		exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 --parallel $threads -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} {*}[compresspipe $outfile] > $outfile.temp
 		file rename -force -- $outfile.temp $outfile
 	}
 }
