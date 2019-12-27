01f290f1141b97a59f562a7a004e36be817a9a4a
Peter De Rijk <Peter.DeRijk@gmail.com>
2019-12-27 09:04:36 +0100

    process_reports_job fix: reports_hsmetrics was missing bam indexfile as dep

--- lib/cg_process_reports.tcl
+++ lib/cg_process_reports.tcl
@@ -107,9 +107,10 @@ proc process_reports_job {args} {
 		if {[inlist $reports hsmetrics] && [jobfileexists $targetfile]} {
 			set dep1 $bamfile
 			set dep2 $targetfile
+			set dep3 $bamfile.[indexext $bamfile]
 			set target $sampledir/reports/hsmetrics-$bamroot.hsmetrics
 			set target2 $sampledir/reports/report_hsmetrics-$bamroot.tsv
-			job reports_hsmetrics-[file tail $bamfile] -optional 1 -deps {$dep1 $dep2} -targets {$target $target2} -vars {bamroot bamfile targetfile} -code {
+			job reports_hsmetrics-[file tail $bamfile] -optional 1 -deps {$dep1 $dep2 $dep3} -targets {$target $target2} -vars {bamroot bamfile targetfile} -code {
 				cg_hsmetrics --sample $bamroot $dep1 $dep2 $target
 				set f [open $target]
 				set header [tsv_open $f]
