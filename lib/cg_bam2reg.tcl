6f30962c651620dd17f1df42b83577b62e37dd74
Peter De Rijk <Peter.DeRijk@gmail.com>
2020-01-02 22:23:09 +0100

    cg bam2reg fix: use distrreg_checkvalue

--- lib/cg_bam2reg.tcl
+++ lib/cg_bam2reg.tcl
@@ -11,15 +11,7 @@ proc bam2reg_job {args} {
 			set compress $value
 		}
 		-distrreg {
-			if {$value in {1 chr chromosome 0}} {
-				set distrreg $value
-			} elseif {[isint $value]} {
-				set distrreg $value
-			} elseif {[file exists $value]} {
-				set distrreg [file_absolute $value]
-			} else {
-				error "unknown value $value for -distrreg, must be a (region) file or one of: chr or 1, 0"
-			}
+			set distrreg [distrreg_checkvalue $value]
 		}
 		-refseq {
 			set refseq $value
