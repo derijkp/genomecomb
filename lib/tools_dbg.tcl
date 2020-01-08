6dc87bd2a9db059e67f247683a3218a0faa4c882
Peter De Rijk <Peter.DeRijk@gmail.com>
2020-01-08 11:37:31 +0100

    added fputsvars debug tool

--- lib/tools_dbg.tcl
+++ lib/tools_dbg.tcl
@@ -7,3 +7,18 @@ proc eputsvars {args} {
 		}
 	}
 }
+
+proc fputsvars {file args} {
+	set o [open $file a]
+	foreach var $args {
+		if {[catch {uplevel [list set $var]} value]} {
+			puts stderr [list unset $var]
+			puts $o [list unset $var]
+		} else {
+			puts stderr [list set $var $value]
+			puts $o [list set $var $value]
+		}
+	}
+	puts stderr "-- fputsvars to $file"
+	close $o
+}
