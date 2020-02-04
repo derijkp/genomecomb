b205e1fd6f99d1b5a72606e591cdf9d5a17145b8
Peter De Rijk <Peter.DeRijk@gmail.com>
2020-02-04 14:15:55 +0100

    cleanup_job fix: only delete dir if empty

--- lib/jobs_tools.tcl
+++ lib/jobs_tools.tcl
@@ -15,7 +15,13 @@ proc cleanup_job {name rmtargets args} {
 		job cleanup-$name-deps$num -optional 1 -deps [list {*}$deps] -vars {rmtargets} \
 		-rmtargets $rmtargets -code {
 			foreach file $rmtargets {
-				catch {file delete -force $file}
+				if {[file isdir $file]} {
+					if {![llength [glob -nocomplain $file/*]]} {
+						catch {file delete -force $file}
+					}
+				} else {
+					catch {file delete -force $file}
+				}
 			}
 		}
 		incr num
