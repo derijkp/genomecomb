1ee6df5257fd2b6a39cc678d564c0a782bad94c3
Peter De Rijk <Peter.DeRijk@gmail.com>
2020-04-17 19:17:11 +0200

    tests fix: commented out albacore test (not being used anymore)

--- tests/nanopore.tcl
+++ tests/nanopore.tcl
@@ -4,14 +4,15 @@ exec tclsh "$0" "$@"
 
 source tools.tcl
 
-test nanopore {basecaller_albacore basic} {
-	file mkdir tmp/test/group/fast5/0
-	file copy {*}[glob data/fast5/*.fast5] tmp/test/group/fast5/0
-	cg basecaller_albacore -stack 1 -v 2 -subdirs 1 -numreads 4 -c r95_450bps_linear.cfg -threads 2 tmp/fastq tmp/test/group
-	cg zcat tmp/fastq/pass_group_0.fastq.gz > tmp/fastq/pass_group_0.fastq
-	cg zcat data/expected-pass_group_0.fastq.gz > tmp/expected.fastq
-	exec diff tmp/fastq/pass_group_0.fastq tmp/expected.fastq
-} {}
+# not testing anymore (albacore is not used anymore)
+#test nanopore {basecaller_albacore basic} {
+#	file mkdir tmp/test/group/fast5/0
+#	file copy {*}[glob data/fast5/*.fast5] tmp/test/group/fast5/0
+#	cg basecaller_albacore -stack 1 -v 2 -subdirs 1 -numreads 4 -c r95_450bps_linear.cfg -threads 2 tmp/fastq tmp/test/group
+#	cg zcat tmp/fastq/pass_group_0.fastq.gz > tmp/fastq/pass_group_0.fastq
+#	cg zcat data/expected-pass_group_0.fastq.gz > tmp/expected.fastq
+#	exec diff tmp/fastq/pass_group_0.fastq tmp/expected.fastq
+#} {}
 
 testsummarize
 
