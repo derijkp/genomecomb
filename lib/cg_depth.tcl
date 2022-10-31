41b6004b8dfdb46a4c1ab095f6741be27cd4eb31
Peter De Rijk <Peter.DeRijk@gmail.com>
2022-10-31 11:30:11 +0100

    added cg depth

---
+++ lib/cg_depth.tcl
@@ -0,0 +1,22 @@
+proc cg_depth {args} {
+	set regionfile {}
+	set max 1000000
+	set opts {}
+	set Q 0
+	set q 0
+	set opts {}
+	cg_options depth args {
+		-max {set max $value}
+		-q {set q $value}
+		-Q {set Q $value}
+		-all {
+			if {[true $value]} {
+				lappend opts -a
+			}
+		}
+	} {bamfile} 1 1 {
+		run samtools depth on a bam/sam file, outputting tsv format
+	}
+	puts [join {chromosome end depth} \t]
+	exec samtools depth {*}$opts -d$max -Q $Q -q $q $bamfile >@ stdout 2>@ stderr
+}
