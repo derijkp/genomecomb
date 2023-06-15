2c4599e32b56129f55cedf7cfee832cfe62dc47d
Peter De Rijk <Peter.DeRijk@gmail.com>
2023-06-15 14:39:25 +0200

    cg_tsvjoin: added -sorted and -default option, fixed sorting to generic (use nat_compar iso loc_compare)

--- lib/cg_tsvjoin.tcl
+++ lib/cg_tsvjoin.tcl
@@ -17,7 +17,7 @@ proc cg_tsvjoin_next {file f idfields poss previdVar prevlineVar idVar lineVar} 
 	if {$r < -1} {return $r}
 	set line [split $line \t]
 	set id [list_sub $line $poss]
-	if {[loc_compare $previd $id] > 0} {
+	if {[nat_compare $previd $id] > 0} {
 		error "file $file not properly sorted on $idfields ($previd comes before $id)"
 	}
 	return $r
@@ -30,6 +30,8 @@ proc cg_tsvjoin {args} {
 	set idfields2 {}
 	set type f
 	set usecomments 1
+	set sorted 0
+	set default {}
 	cg_options tsvjoin args {
 		-pre1 {set pre1 $value}
 		-pre2 {set pre2 $value}
@@ -52,14 +54,14 @@ proc cg_tsvjoin {args} {
 				}
 			}
 		}
+		-default {
+			set default $value
+		}
+		-sorted {
+			set sorted $value
+		}
 	} {file1 file2 resultfile} 2 3
 	if {![llength $idfields2]} {set idfields2 $idfields1}
-	if {[info exists resultfile]} {
-		set o [open $resultfile w]
-	} else {
-		set o stdout
-	}
-
 	catch {close $f1} ; catch {close $f2}
 	set f1 [gzopen $file1]
 	set header1 [tsv_open $f1 comments1]
@@ -74,9 +76,9 @@ proc cg_tsvjoin {args} {
 	set poss2 [list_cor $header2 $idfields2]
 	if {[inlist $poss2 -1]} {error "missing idfields in $file2: [list_sub $idfields2 [list_find $poss2 -1]]"}
 	set remain2 [list_lremove $header2 $idfields2]
-	set sdefline1 [list_fill [llength $header1] {}]
+	set sdefline1 [list_fill [llength $header1] $default]
 	set defline1 [join $sdefline1 \t]
-	set sdefline2 [list_fill [llength $remain2] {}]
+	set sdefline2 [list_fill [llength $remain2] $default]
 	set defline2 [join $sdefline2 \t]
 	if {[llength [list_common $remain2 [list_lremove $header1 $idfields1]]]} {
 		if {$pre1 eq $pre2} {
@@ -98,6 +100,22 @@ proc cg_tsvjoin {args} {
 			lappend newheader ${pre2}$field
 		}
 	}
+	if {!$sorted} {
+		gzclose $f1 ; gzclose $f2
+		set tempfile1 [tempfile].zst
+		set tempfile2 [tempfile].zst
+		cg select -s $idfields1 $file1 $tempfile1
+		cg select -s $idfields1 $file2 $tempfile2
+		set f1 [gzopen $tempfile1]
+		set header1 [tsv_open $f1 comments1]
+		set f2 [gzopen $tempfile2]
+		set header2 [tsv_open $f2 comments2]
+	}
+	if {[info exists resultfile]} {
+		set o [wgzopen $resultfile]
+	} else {
+		set o stdout
+	}
 	if {$usecomments eq "1"} {
 		puts -nonewline $o $comments1
 	} elseif {$usecomments eq "2"} {
@@ -113,18 +131,18 @@ proc cg_tsvjoin {args} {
 	while 1 {
 		# putsvars previd1 prevline1 id1 line1
 		# putsvars previd2 prevline2 id2 line2
-		set c [loc_compare $id1 $id2]
+		set c [nat_compare $id1 $id2]
 		if {$c == 0} {
 			puts $o [join $line1 \t]\t[join [list_sub $line2 -exclude $poss2] \t]
 			set r2 [cg_tsvjoin_next $file2 $f2 $idfields2 $poss2 previd2 prevline2 id2 line2]
 			set r1 [cg_tsvjoin_next $file1 $f1 $idfields2 $poss1 previd1 prevline1 id1 line1]
 			if {$r1 == -1 || $r2 == -1} break
 			while 1 {
-				if {[loc_compare $previd1 $id2] == 0} {
+				if {[nat_compare $previd1 $id2] == 0} {
 					puts $o [join $prevline1 \t]\t[join [list_sub $line2 -exclude $poss2] \t]
 					set r2 [cg_tsvjoin_next $file2 $f2 $idfields2 $poss2 previd2 prevline2 id2 line2]
 					if {$r2 < -1} break
-				} elseif {[loc_compare $id1 $previd2] == 0} {
+				} elseif {[nat_compare $id1 $previd2] == 0} {
 					puts $o [join $line1 \t]\t[join [list_sub $prevline2 -exclude $poss2] \t]
 					set r1 [cg_tsvjoin_next $file1 $f1 $idfields2 $poss1 previd1 prevline1 id1 line1]
 					if {$r1 < -1} break
