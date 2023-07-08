893cc17d88ea4dd86eae48905b8dba8d6a38e5aa
Peter De Rijk <Peter.DeRijk@gmail.com>
2023-07-08 08:59:36 +0200

    cg sc_barcodes: parallellized barcode matching (precalculate before cellbarcoding step)

--- lib/cg_sc.tcl
+++ lib/cg_sc.tcl
@@ -15,7 +15,7 @@ proc find_barcodes {fastq resultfile sumresultfile adaptorseq {barcodesize 16} {
 	set reffile [tempfile].fa
 	set ref [sc_barcodes_ref $reffile $adaptorseq]
 	set sam [tempfile].sam
-	set sam $resultfile.sam
+	# set sam $resultfile.sam
 	catch_exec minimap2 -a --secondary=no -x map-ont -t 4 -n 1 -m 1 -k 5 -w 1 -s 20 $ref $fastq > $sam 2>@ stderr
 	# cg sam2tsv $sam | cg select -g chromosome
 	# exec ~/dev/genomecomb/bin/sc_getbarcodes adapter $begin 16 10 < $sam > temp
@@ -68,6 +68,7 @@ proc find_barcodes {fastq resultfile sumresultfile adaptorseq {barcodesize 16} {
 	cg select -overwrite 1 -s count $sumresultfile.temp $sumresultfile.temp2
 	file rename -force $sumresultfile.temp2 $sumresultfile
 	file delete $sumresultfile.temp
+	file delete $sam
 }
 
 proc ali_ident_matrix {} {
@@ -115,6 +116,11 @@ proc sc_barcodes_job args {
 	set adaptorseq CTACACGACGCTCTTCCGATCT
 	set barcodesize 16
 	set umisize 12
+	set costcutoff 2
+	set bcparts 50
+	set maxcells 100000
+	set mincells 1000
+	set cutoff 20
 	cg_options sc_barcodes args {
 		-whitelist {
 			set whitelist $value
@@ -125,8 +131,17 @@ proc sc_barcodes_job args {
 		-barcodesize {
 			set barcodesize	$value
 		}
+		-maxcost {
+			set costcutoff [expr {$value+1}]
+		}
+		-cutoff {
+			set cutoff $value
+		}
 		-umisize {
 			set umisize $value
+		}
+		-bcparts {
+			set bcparts $value
 		}
 	} {fastqdir resultdir} 1 2 {
 		cg sc_barcodes finds (cell)barcodes and umis from 10x single cell sequencing in nanopore reads.
@@ -140,8 +155,7 @@ proc sc_barcodes_job args {
 	set fastqdir [file_absolute $fastqdir]
 	if {![info exists resultdir]} {set resultdir [file dir $fastqdir]}
 	set resultdir [file_absolute $resultdir]
-	set sampledir [file dir $resultdir]
-	set sample [file tail $sampledir]
+	set sample [file tail $resultdir]
 	# set workdir [shadow_workdir $resultdir]
 	# set workdir [workdir $resultdir]
 	set cleanupfiles {}
@@ -158,7 +172,13 @@ proc sc_barcodes_job args {
 		set target $resultdir/barcodes/$root.barcodes.tsv
 		set target2 $resultdir/barcodes/$root.summary_barcodes.tsv
 		lappend summaries $target2
-		job find_barcodes-$sample-[file tail $fastq] -deps {
+		job find_barcodes-$sample-[file tail $fastq] -skip [list \
+			$resultdir/mergedbarcodes.tsv.zst \
+			$resultdir/barcode2celbarcode.tsv \
+			$resultdir/reads_per_cell.tsv \
+			$resultdir/bcfastq/$root-sc.fastq.gz \
+			$resultdir/barcodeinfo/$root.barcodes.tsv \
+		] -deps {
 			$fastq
 		} -targets {
 			$target $target2
@@ -171,24 +191,37 @@ proc sc_barcodes_job args {
 		}
 	}
 	# merge barcodes and find cell barcodes
-	set summaries [jobglob $resultdir/barcodes/*.summary_barcodes.tsv]
 	set barcodefiles [jobglob $resultdir/barcodes/*.barcodes.tsv]
-	job merge_barcodes-$sample -deps [list {*}$barcodefiles {*}$summaries] -targets {
+	set mergedbarcodesfile $resultdir/mergedbarcodes.tsv.zst
+	job merge_barcodes-$sample -deps $barcodefiles -targets {
 		$resultdir/mergedbarcodes.tsv.zst
-		$resultdir/barcode2celbarcode.tsv
-		$resultdir/reads_per_cell.tsv
+		$resultdir/barcode_cutoff-info.tsv
 	} -vars {
-		barcodefiles summaries resultdir whitelist
-	} -procs {
-		ali_ident_matrix
+		barcodefiles resultdir whitelist barcodesize threads mergedbarcodesfile
 	} -code {
 
-		set mergedbarcodesfile $resultdir/mergedbarcodes.tsv.zst
-		set tempfile [tempfile].zst
+		#
+		# load whitelist
+		catch {close $f}
+		set f [gzopen $whitelist]
+		unset -nocomplain wa
+		while 1 {
+			if {[gets $f barcode] == -1} break
+#			if {![info exists a($barcode)]} continue
+#			if {$a($barcode) <= 2} continue
+			set wa($barcode) 1
+		}
+		close $f
+		# llength [array names wa]
+
+		#
+		# make mergedbarcodes
+		set tempfile $resultdir/mergedbarcodes-prefile.zst
+		# set tempfile [tempfile].zst
 		cg zcat {*}$barcodefiles | cg select -s {barcode * umi *} | cg zst > $tempfile
 		catch {close $o} ; catch {close $f}
 		set o [wgzopen $mergedbarcodesfile.temp.zst]
-		puts $o barcode\tcount\tumicount
+		puts $o barcode\tcount\tumicount\twhitelist
 		set f [gzopen $tempfile]
 		set header [tsv_open $f]
 		if {$header ne "id barcode umi start strand polyA"} {error "wrong file $file?"}
@@ -200,7 +233,12 @@ proc sc_barcodes_job args {
 			if {$barcode ne $prevbarcode} {
 				incr a($prevbarcode) $count
 				if {$count > 0 && $barcode ne ""} {
-					puts $o $prevbarcode\t$count\t$umicount
+					if {[info exists wa($prevbarcode)]} {
+						set white 1
+					} else {
+						set white 0
+					}
+					puts $o $prevbarcode\t$count\t$umicount\t$white
 				}
 				set prevbarcode $barcode
 				set prevumi $umi
@@ -219,108 +257,198 @@ proc sc_barcodes_job args {
 		cg select -overwrite 1 -s -count $mergedbarcodesfile.temp.zst $mergedbarcodesfile.temp2.zst
 		file rename -force $mergedbarcodesfile.temp2.zst $mergedbarcodesfile
 		file delete $mergedbarcodesfile.temp.zst
-#		# read counts into a after file is made already
-#		unset -nocomplain a
-#		set f [gzopen $resultdir/mergedbarcodes.tsv.zst]
-#		set header [tsv_open $f]
-#		if {$header ne "barcode count umicount"} {error "wrong file $file?"}
-#		while {[gets $f line] != -1} {
-#			foreach {barcode count} [split $line \t] break
-#			incr a($barcode) $count
-#		}
-#		gzclose $f
-
 		#
-		# load whitelist
+		# barcode_cutoff-info.tsv
 		catch {close $f}
-		set f [gzopen $whitelist]
-		unset -nocomplain wa
+		set f [gzopen $mergedbarcodesfile]
+		set header [tsv_open $f]
+		set line [gets $f]
+		foreach {barcode count umicount white} [split $line \t] break
+		if {$barcode eq ""} {
+			set line [gets $f]
+			foreach {barcode count umicount white} [split $line \t] break
+		}
+		set max $count
+		if {$cutoff ne ""} {
+			set ucutoff $cutoff
+		} else {
+			# if cutoff not set directly, calc as fraction from max
+			set ucutoff [expr {$max*0.05}]
+			# go to at least 100
+			if {$ucutoff > 100} {
+				set ucutoff 100
+			}
+		}
+		set numlines 1
+		if {$white} {set whitenum 1} else {set whitenum 0}
+		set minlen [expr {$barcodesize-2}]
 		while 1 {
-			if {[gets $f barcode] == -1} break
-			if {![info exists a($barcode)]} continue
-			if {$a($barcode) <= 2} continue
-			set wa($barcode) $a($barcode)
+			if {[gets $f line] == -1} break
+			foreach {barcode count umicount white} [split $line \t] break
+			if {$barcode eq "" || $count <= 2 || [string length $barcode] <= $minlen} continue
+			if {$white} {incr whitenum $white}
+			if {$whitenum > $maxcells} break
+			if {$count < $ucutoff && $whitenum > $mincells} break
+			incr numlines
 		}
-		close $f
-		#
-		# match to whitelist barcodes
-		# start from highest count
-		# prepare matrix and list for ali_cost
-		package require BioTcl
-		set matrix [tempfile].mat
-		file_write $matrix [ali_ident_matrix]
-		set list {}
-		foreach barcode [array names a] {
-			if {$a($barcode) <= 2} continue
-			lappend list $barcode $barcode
+		gzclose $f
+		file_write $resultdir/barcode_cutoff-info.tsv [join [list \
+			key\tvalue \
+			max\t$max \
+			cutoff\t$cutoff \
+			ucutoff\t$ucutoff \
+			numlines\t$numlines \
+			whitenum\t$whitenum \
+		] \n]
+	}
+	mkdir $resultdir/barcodes.temp
+	set barcode_matches {}
+	for {set part 1} {$part <= $bcparts} {incr part} {
+		set target $resultdir/barcodes.temp/barcode_matches-$part.tsv.zst
+		lappend barcode_matches $target
+		job sc_findbcmatches-barcode_matches-$part -skip [list $resultdir/barcode2celbarcode.tsv $resultdir/reads_per_cell.tsv] \
+		-deps {
+			$resultdir/mergedbarcodes.tsv.zst
+			$resultdir/barcode_cutoff-info.tsv
+		} -targets {
+			$target
+		} -vars {
+			part costcutoff bcparts barcodesize resultdir
+		} -procs {
+			ali_ident_matrix
+		} -code {
+			# get barcode list
+			unset -nocomplain wa
+			set f [gzopen $resultdir/mergedbarcodes.tsv.zst]
+			set header [tsv_open $f]
+			set minlen [expr {$barcodesize-2}]
+			set list {}
+			while 1 {
+				if {[gets $f line] == -1} break
+				foreach {barcode count umicount whitelist} [split $line \t] break
+				if {$barcode eq "" || $count <= 2 || [string length $barcode] <= $minlen} continue
+				if {$whitelist} {set wa($barcode) 1}
+				lappend list $barcode
+			}
+			gzclose $f
+			# prepare matrix and list for ali_cost
+			package require BioTcl
+			set matrix [tempfile].mat
+			file_write $matrix [ali_ident_matrix]
+			# get part to test
+			array set infoa [file_read $resultdir/barcode_cutoff-info.tsv]
+			set partsize [expr {$infoa(numlines)/$bcparts}]
+			set pos [expr {($part-1)*$partsize}]
+			set todo [lrange $list $pos [expr {$pos+$partsize-1}]]
+			# get and write matches
+			set o [wgzopen $target.temp.zst]
+			puts $o barcode\tmatches
+			set num 0
+			foreach barcode $todo {
+				puts [incr num].$barcode
+				if {![info exists wa($barcode)]} continue
+				set matches [ali_cost -names 0 -cutoff $costcutoff -matrix $matrix $barcode $list]
+				puts $o $barcode\t$matches
+			}
+			gzclose $o
+			file rename $target.temp.zst $target
 		}
-		#
+	}
+	job barcode2celbarcode-$sample -deps [list \
+		$mergedbarcodesfile \
+		$resultdir/barcode_cutoff-info.tsv \
+		{*}$barcode_matches \
+	] -targets {
+		$resultdir/barcode2celbarcode.tsv
+		$resultdir/reads_per_cell.tsv
+	} -vars {
+		barcode_matches resultdir whitelist barcodesize threads maxcells mincells
+	} -code {
+		if 0 {
+			set f [gzopen $mergedbarcodesfile]
+			set h [tsv_open $f]
+			while {[gets $f line] != -1} {
+				foreach {barcode count umicount whitelist} [split $line \t] break
+				set a($barcode) $count
+				set wa($barcode) $whitelist
+			}
+			close $f
+		}
 
-		set barcode2celbarcodefile $resultdir/barcode2celbarcode.tsv
+		# load counts in a (counts) and ua (umicounts)
 		set f [gzopen $mergedbarcodesfile]
 		set header [tsv_open $f]
 		unset -nocomplain a
+		unset -nocomplain ua
 		while {[gets $f line] != -1} {
-			foreach {barcode count} [split $line \t] break
+			foreach {barcode count umicount} [split $line \t] break
 			set a($barcode) $count
+			set ua($barcode) $umicount
 		}
-		
-		catch {close $f} ; catch {close $o}
-		set f [gzopen $mergedbarcodesfile]
-		set o [open $barcode2celbarcodefile.temp w]
-		puts $o barcode\tcellbarcode\tcount\twhitelistmatch
-		set header [tsv_open $f]
-		# make matches with max 1 diff
+		gzclose $f
+
+		# matches with max 1 diff already done in barcodes.temp/barcode_matches-$part.tsv.zst
 		# even if a match is also in whitelist we take it as an error in the higher count barcode:
 		# this is likely to come across, while it is highly unlikely to have 2 1-diff barcodes in actual cells
 		# unset a($match) to indicate this one is already taken (assign to highest)
-		set max {}
+		set barcode2celbarcodefile $resultdir/barcode2celbarcode.tsv
+		catch {close $f} ; catch {close $o}
+		set o [open $barcode2celbarcodefile.temp w]
+		puts $o barcode\tcellbarcode\tcount\tumicount\twhitelistmatch
 		# min size of barcode (start)
-		set cutoff 200
 		set num 0
-		# maximum number of barcodes
-		set maxnum 100000
-		while 1 {
-			puts [incr num].$line
-			if {[gets $f line] == -1} break
-			foreach {barcode count} [split $line \t] break
-			if {![info exists wa($barcode)]} {
-				# not in whitelist, but do we really want to skip these?
-				continue
-			}
-			if {![info exists a($barcode)]} {
-				# already used (as a match)
-				continue
-			}
-			if {$cutoff eq ""} {
-				if cutoff not set directly, calc as fraction from max
-				set max $a($barcode)
-				set cutoff [expr {$max*0.05}]
-			}
-			if {$count < $cutoff} break
-			if {$num > $maxnum} break
-			time {set matches [ali_cost -cutoff 2 -matrix $matrix $barcode $list]}
-			foreach {match temp} $matches {
-				if {![info exists a($match)]} continue
-				if {$match eq $barcode} {
-					set wmatch 2
-				} elseif {[info exists wa($match)]} {
-					set wmatch 1
-				} else {
-					set wmatch 0
+		# cutoff info
+		array set infoa [file_read $resultdir/barcode_cutoff-info.tsv]
+		set max $infoa(max)
+		foreach barcode_match $barcode_matches {
+			set fm [gzopen $barcode_match]
+			set header [tsv_open $fm]
+			while 1 {
+				if {[gets $fm line] == -1} break
+				foreach {barcode matches} [split $line \t] break
+				if {![info exists a($barcode)]} {
+					# already used (as a match)
+					continue
 				}
-				puts $o $match\t$barcode\t$a($match)\t$wmatch
-				unset a($match)
+				puts "[incr num].$barcode ($a($barcode))"
+				if {![info exists wa($barcode)]} {
+					# not in whitelist, but do we really want to skip these if high counts?
+					continue
+				}
+				set count $a($barcode)
+				if {$count < $cutoff && $num > $mincells} break
+				if {$num > $maxcells} break
+				foreach {match temp} $matches {
+					if {![info exists a($match)]} continue
+					if {$match eq $barcode} {
+						set wmatch 2
+					} elseif {[info exists wa($match)]} {
+						set wmatch 1
+					} else {
+						set wmatch 0
+					}
+					puts $o $match\t$barcode\t$a($match)\t$ua($match)\t$wmatch
+					unset a($match)
+					unset ua($match)
+				}
 			}
+			gzclose $fm
 		}
 		close $o
-		gzclose $f
+
 		file rename -force $barcode2celbarcodefile.temp $barcode2celbarcodefile
 		# cg select -g cellbarcode barcode2celbarcode.tsv	| cg select -g all
 		# cg select -g all -gc 'sum(count)' barcode2celbarcode.tsv
+		# readcounts
 		cg select -g cellbarcode -gc {sum(count)} $barcode2celbarcodefile | cg select -f {cellbarcode count=$sum_count} -s -sum_count > $resultdir/reads_per_cell.tsv.temp
 		file rename -force $resultdir/reads_per_cell.tsv.temp $resultdir/reads_per_cell.tsv
+		# umicounts
+		cg select -g cellbarcode -gc {sum(umicount)} $barcode2celbarcodefile | cg select -f {cellbarcode count=$sum_umicount} -s -sum_umicount > $resultdir/umis_per_cell.tsv.temp
+		file rename -force $resultdir/umis_per_cell.tsv.temp $resultdir/umis_per_cell.tsv
 		unset -nocomplain a ; unset -nocomplain wa
+		# foreach barcode_match $barcode_matches {
+		# file delete $barcode_match
+		# }
 	}
 
 	# apply found barcodes to make barcodeinfo and bcfastq
@@ -352,7 +480,7 @@ proc sc_barcodes_job args {
 			catch {close $f}
 			set f [open $resultdir/barcode2celbarcode.tsv]
 			set header [tsv_open $f]
-			if {$header ne {barcode cellbarcode count whitelistmatch}} {
+			if {[lrange $header 0 2] ne {barcode cellbarcode count}} {
 				error "wrong header for file $resultdir/barcode2celbarcode.tsv"
 			}
 			while 1 {
@@ -372,7 +500,7 @@ proc sc_barcodes_job args {
 			if {$header ne {id barcode umi start strand polyA}} {error "wrong header for file $dep2"}
 			set fqo [wgzopen $target]
 			set fio [wgzopen $target2]
-			puts $fio [join {id cellbarcode oribarcode umi start strand polyA} \t]
+			puts $fio [join {id cellbarcode umi oribarcode start strand polyA} \t]
 			while 1 {
 				set fbread [gets $fb line]
 				set fqread [gets $ff fqname]
