09e1b1fc10b06dbc22961b7320dffe9b351246d1
Peter De Rijk <Peter.DeRijk@gmail.com>
2023-11-22 09:44:34 +0100

    feature pb_combine_job: added combination of colinfo files

--- lib/pseudobulk.tcl
+++ lib/pseudobulk.tcl
@@ -1,3 +1,21 @@
+proc pb_combine_cat_colinfo {result files} {
+	file copy [lindex $files 0] $result.temp
+	set o [open $result.temp a]
+	foreach file [lrange $files 1 end] {
+		set f [open $file]
+		set header [tsv_open $f]
+		set pos [lsearch $header fieldtype]
+		while {[gets $f line] != -1} {
+			set type [lindex [split $line \t] $pos]
+			if {$type ne "data"} continue
+			puts $o $line
+		}
+		close $f
+	}
+	close $o
+	file rename -force $result.temp $result
+}
+
 proc pb_combine_job {projectdir sc_celltyper {iso_match {}}} {
 	upvar job_logdir job_logdir
 	# combined analysis
@@ -9,8 +27,9 @@ proc pb_combine_job {projectdir sc_celltyper {iso_match {}}} {
 	} else {
 		set root ${sc_celltyper}-$exproot
 	}
-	set pbisoformfiles [bsort [jobglob samples/*/pb_isoform_counts-${sc_celltyper}-*.tsv]]	
-	set pbisoformfiles [list_lremove $pbisoformfiles [jobglob samples/*/pb_isoform_counts-${sc_celltyper}-*.colinfo.tsv]]	
+	set pbisoformfiles [bsort [jobglob samples/*/pb_isoform_counts-${sc_celltyper}-*.tsv]]
+	set pbisoformcolinfofiles [jobglob samples/*/pb_isoform_counts-${sc_celltyper}-*.colinfo.tsv]
+	set pbisoformfiles [list_lremove $pbisoformfiles $pbisoformcolinfofiles]	
 	if {[llength $pbisoformfiles]} {
 		job pb_compar-pb_isoform_counts-$root \
 		-deps $pbisoformfiles \
@@ -23,20 +42,41 @@ proc pb_combine_job {projectdir sc_celltyper {iso_match {}}} {
 			cg multitranscript -match $iso_match $isoformcounts.temp.zst {*}$pbisoformfiles
 			file rename -force $isoformcounts.temp.zst $isoformcounts.zst
 		}
+		job pb_compar-pb_isoform_counts_colinfo-$root \
+		-deps $pbisoformcolinfofiles \
+		-targets {
+			compar/pb_isoform_counts-$root.colinfo.tsv
+		} -vars {
+			pbisoformcolinfofiles exproot root sc_celltyper iso_match
+		} -code {
+			set isoformcounts_colinfo compar/pb_isoform_counts-$root.colinfo.tsv
+			pb_combine_cat_colinfo $isoformcounts_colinfo $pbisoformcolinfofiles
+		}
 	}
 	set pbgenefiles [bsort [jobglob samples/*/pb_gene_counts-${sc_celltyper}-*.tsv]]
-	set pbgenefiles [list_lremove $pbgenefiles [jobglob samples/*/pb_gene_counts-${sc_celltyper}-*.colinfo.tsv]]	
+	set pbgenecolinfofiles [jobglob samples/*/pb_gene_counts-${sc_celltyper}-*.colinfo.tsv]
+	set pbgenefiles [list_lremove $pbgenefiles $pbgenecolinfofiles]	
 	if {[llength $pbgenefiles]} {
 		job pb_compar-gene_counts-$root \
 		-deps $pbgenefiles \
 		-targets {
-			compar/gene_counts-$root.tsv
+			compar/pb_gene_counts-$root.tsv
 		} -vars {
 			pbgenefiles exproot root sc_celltyper iso_match
 		} -code {
 			set genecounts compar/pb_gene_counts-$root.tsv
 			cg_multigene -match $iso_match $genecounts.temp.zst {*}$pbgenefiles
 			file rename -force $genecounts.temp.zst $genecounts.zst
+		}
+		job pb_compar-pb_gene_counts_colinfo-$root \
+		-deps $pbgenecolinfofiles \
+		-targets {
+			compar/pb_gene_counts-$root.colinfo.tsv
+		} -vars {
+			pbgenecolinfofiles exproot root sc_celltyper iso_match
+		} -code {
+			set genecounts_colinfo compar/pb_gene_counts-$root.colinfo.tsv
+			pb_combine_cat_colinfo $genecounts_colinfo $pbgenecolinfofiles
 		}
 	}
 }
