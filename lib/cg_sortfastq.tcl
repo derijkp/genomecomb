a81ccf228e2dc7b733ef63c5e9c05a5c8dfbb3fb
Peter De Rijk <Peter.DeRijk@gmail.com>
2023-11-29 15:15:09 +0100

    fixes fastq comments: fixes in cg_sortfastq, cg_tsv2fastq, tests pmulticompar and vcf2tsv

--- lib/cg_sortfastq.tcl
+++ lib/cg_sortfastq.tcl
@@ -4,11 +4,14 @@ proc cg_sortfastq {args} {
 		-threads {set threads $value}
 	} {infile outfile} 0 2
 	if {![info exists infile]} {
-		exec paste - - - - | gnusort8 --parallel $threads-T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} <@ stdin >@ stdout
+		exec cg fastq2tsv | cg select -s name | cg tsv2fastq <@ stdin >@ stdout
+		# exec paste - - - - | gnusort8 --parallel $threads-T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} <@ stdin >@ stdout
 	} elseif {![info exists outfile]} {
-		exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 --parallel $threads -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} >@ stdout
+		exec cg fastq2tsv $infile | cg select -s name | cg tsv2fastq >@ stdout
+		# exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 --parallel $threads -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} >@ stdout
 	} else {
-		exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 --parallel $threads -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} {*}[compresspipe $outfile] > $outfile.temp
+		# exec {*}[gzcat $infile] $infile | paste - - - - | gnusort8 --parallel $threads -T [scratchdir] -t \t -V -s -k 1,1 | tr {\t} {\n} {*}[compresspipe $outfile] > $outfile.temp
+		exec cg fastq2tsv $infile | cg select -s name | cg tsv2fastq {*}[compresspipe $outfile] > $outfile.temp
 		file rename -force -- $outfile.temp $outfile
 	}
 }
