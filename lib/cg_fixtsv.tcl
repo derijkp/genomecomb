571d8a78dc4e2bd6b10e250805bcf2223e512124
Peter De Rijk <Peter.DeRijk@gmail.com>
2024-04-12 01:17:25 +0200

    feature cg_fixtsv: remove duplicate fields from tsv file

--- lib/cg_fixtsv.tcl
+++ lib/cg_fixtsv.tcl
@@ -1,10 +1,21 @@
-proc cg_fixtsv {file outfile} {
+proc cg_fixtsv {args} {
+	cg_options fixtsv args {
+	} {file outfile} 2 2
 	set f [gzopen $file]
 	set header [tsv_open $f comment]
 	# set poss [tsv_basicfields $header 4 0]
 	set o [open $outfile.temp w]
 	puts -nonewline $o $comment
-	puts $o [join $header \t]
+	set rheader [list_remdup $header]
+	if {[llength $rheader] < [llength $header]} {
+		set cor [list_cor $header $rheader]
+		set duplicates [list_sub $header -exclude $cor] 
+		puts "header shows duplicate fields ([join $duplicates ", "]), removing"
+		puts $o [join [list_sub $header $cor] \t]
+	} else {
+		set cor {}
+		puts $o [join $header \t]
+	}
 	set tline [gets $f]
 	set line [split $tline \t]
 	# foreach {pchr pbegin pend ptype} [list_sub $line $poss] break
@@ -20,9 +31,15 @@ proc cg_fixtsv {file outfile} {
 			} else {
 				set line [lrange $line 0 [expr {$len-1}]]
 			}
+			if {[llength $cor]} {set line [list_sub $line $cor]}
 			puts $o [join $line \t]
 		} else {
-			puts $o $tline
+			if {[llength $cor]} {
+				set line [list_sub $line $cor]
+				puts $o [join $line \t]
+			} else {
+				puts $o $tline
+			}
 		}
 		if {[eof $f]} break
 		set tline [gets $f]
